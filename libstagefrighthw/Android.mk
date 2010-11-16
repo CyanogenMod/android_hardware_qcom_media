1910094a3149a0e72d74f83077f636d91e334cc9
Andreas Huber <andih@google.com>
2010-11-16 12:46:53 -0800

    Remove legacy renderers.
    Change-Id: Idb8650aad6e29ef993fea63c40b2459170bb0773

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -20,9 +20,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    stagefright_surface_output_msm7x30.cpp \
     QComOMXPlugin.cpp                      \
-    QComHardwareRenderer.cpp
 
 LOCAL_CFLAGS := $(PV_CFLAGS_MINUS_VISIBILITY)
 
