f1f18b0b5aadb0aeb23906ad3a854d6edc62dda7
Mathias Agopian <mathias@google.com>
2011-03-25 18:48:19 -0700

    remove unneeded library dependency
    Change-Id: Ic4aceac3d458e9d9957c40d8119326fe4daf862d

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -33,7 +33,6 @@ LOCAL_SHARED_LIBRARIES :=       \
         libcutils               \
         libdl                   \
         libui                   \
-        libsurfaceflinger_client\
 
 LOCAL_MODULE := libstagefrighthw
 
