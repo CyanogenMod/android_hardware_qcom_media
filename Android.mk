880e28473445e21bf333ccd8cd453537cb233443
Diogo Ferreira <defer@cyanogenmod.com>
2011-11-29 11:42:16 +0000

    qcom-media: Only include products for MSM boards
    This change properly excludes these products from being inherited
    by non-qcom devices.
    
    Beware that the MSM7K_BOARD_PLATFORMS is not in place yet and
    if you want to try these with currently unsupported devices you
    will have to define this yourself.
    
    Change-Id: I03619292e91ec904397f557e5497d46462d99025

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-include $(QCOM_MEDIA_ROOT)/audio/Android.mk
+ifeq ($(call is-board-platform-in-list,$(MSM7K_BOARD_PLATFORMS)),true)
+	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
+endif
