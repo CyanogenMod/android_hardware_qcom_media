963381d293c865488c3040d7e16fdd59e9294cfb
toastcfh <toastcfh@gmail.com>
2011-12-18 21:12:42 -0500

    media: clean up and add some BoardConfig options for building
    - libstagefrighthw: renamed to libstagefrghthw-7x30 because its only for that platform
    - libstagefrighthw-8x60: add library for 8x60 platforms
    - audio: dont build audio if BOARD_PREBUILT_LIBAUDIO is true
    
    Change-Id: Ie57c75ad8726a8edb28bdde3436063e98f184efd

--- Android.mk
+++ Android.mk
@@ -1,9 +1,15 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	media-hals := libstagefrighthw
-	include $(call all-named-subdir-makefiles,$(media-hals))
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true) 
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+	=include $(QCOM_MEDIA_ROOT)/libstagefrighthw-7x30/Android.mk
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+	=include $(QCOM_MEDIA_ROOT)/libstagefrighthw-8x60/Android.mk
+endif
+ifneq ($(BOARD_PREBUILT_LIBAUDIO),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
+endif
 ifeq ($(BOARD_USES_QCOM_LIBS),true)
 	include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 	include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
