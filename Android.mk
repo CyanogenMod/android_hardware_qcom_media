89f56e3cc4dc8755e3121997683fea282a5b9869
toastcfh <toastcfh@gmail.com>
2011-12-18 21:12:42 -0500

    media: clean up and add some BoardConfig options for building
    - libstagefrighthw: add libraries for both msm8x60 and msm7x30 targets
    - audio: dont build audio if BOARD_PREBUILT_LIBAUDIO is true
    
    Change-Id: I8d2bea4931594980f10497924320606debf0152f

--- Android.mk
+++ Android.mk
@@ -1,9 +1,11 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	media-hals := libstagefrighthw
-	include $(call all-named-subdir-makefiles,$(media-hals))
+	include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+
+ifneq ($(BOARD_PREBUILT_LIBAUDIO),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
+endif
 ifeq ($(BOARD_USES_QCOM_LIBS),true)
 	include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 	include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
