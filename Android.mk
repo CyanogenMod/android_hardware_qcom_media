c2ce8ee1f12493c307c7a4b6d76c56325231c99f
toastcfh <toastcfh@gmail.com>
2011-12-18 21:12:42 -0500

    media: clean up and add some BoardConfig options for building
    - libstagefrighthw: add libraries for both msm8x60 and msm7x30 targets
    - audio: dont build audio if BOARD_PREBUILT_LIBAUDIO is true
    
    Change-Id: I8d2bea4931594980f10497924320606debf0152f

--- Android.mk
+++ Android.mk
@@ -3,7 +3,10 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 	media-hals := libstagefrighthw
 	include $(call all-named-subdir-makefiles,$(media-hals))
+
+ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
+endif
 ifeq ($(BOARD_USES_QCOM_LIBS),true)
 	include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 	include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -1,41 +1,9 @@
-#
-# Copyright (C) 2010 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+SF_HW_ROOT := $(call my-dir)
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+    include $(SF_HW_ROOT)/msm7x30/Android.mk
+endif
 
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    QComOMXPlugin.cpp                      \
-
-LOCAL_CFLAGS := $(PV_CFLAGS_MINUS_VISIBILITY)
-
-LOCAL_C_INCLUDES:= \
-        $(TOP)/frameworks/base/include/media/stagefright/openmax
-
-LOCAL_SHARED_LIBRARIES :=       \
-        libbinder               \
-        libutils                \
-        libcutils               \
-        libdl                   \
-        libui                   \
-
-LOCAL_MODULE := libstagefrighthw
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+    include $(SF_HW_ROOT)/msm8x60/Android.mk
 endif
