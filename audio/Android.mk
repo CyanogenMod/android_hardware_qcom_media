0eb22bcf99f418ee05c590b565c7b0fed7473e23
Twisted Playground <twistedumbrella@gmail.com>
2011-12-19 22:13:14 -0500

    qcom:media: qsd8k / cleanup libstagefrighthw, audio
    Fixes to mm-core and mm-video alignment courtesy of aeroevan.
    Add qsd8k audio files (from msm7k) courtesy of Evervolv folder
    inclusion.
    Folders and makefile definitions to match BOARD_PLATFORM standards used
    throughout system.
    Revert changes from 8660 to 8x60 in upstream source.
    
    8660, unlike the 7x series, currently maintains the full number
    classification and shows no indication of upcoming conversion to the
    same standard.

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,6 +1,6 @@
 AUDIO_HW_ROOT := $(call my-dir)
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm8x60)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     include $(AUDIO_HW_ROOT)/msm8x60/Android.mk
 endif
 
@@ -10,4 +10,8 @@ endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
     include $(AUDIO_HW_ROOT)/msm7x27a/Android.mk
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    include $(AUDIO_HW_ROOT)/qsd8k/Android.mk
 endif
