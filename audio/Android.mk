e75cf40b008554e9fd0a012ef0d8b653c3df73cf
Twisted Playground <twistedumbrella@gmail.com>
2011-12-19 20:02:01 -0500

    qcom:media: qsd8k / cleanup libstagefrighthw, audio
    Fixes to mm-core and mm-video alignment courtesy of aeroevan.
    Add qsd8k audio files (from msm7k) courtesy of Evervolv folder
    inclusion.
    Cleanup msm7x30 and msm8x60 audio build by calling QCOM_HARDWARE flags.
    Folders and makefile definitions to match BOARD_PLATFORM standards used
    throughout system.
    Remove prebuilt libaudio ifneq, as libaudio is not the HAL created by
    qcom hardware.

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,5 +1,9 @@
 AUDIO_HW_ROOT := $(call my-dir)
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+    GLOBAL_CFLAGS += -DQCOM_HARDWARE
+endif
+
 ifeq ($(TARGET_BOARD_PLATFORM),msm8x60)
     include $(AUDIO_HW_ROOT)/msm8x60/Android.mk
 endif
@@ -10,4 +14,8 @@ endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
     include $(AUDIO_HW_ROOT)/msm7x27a/Android.mk
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    include $(AUDIO_HW_ROOT)/qsd8k/Android.mk
 endif
