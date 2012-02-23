a07be3832db235361129c060951706e6b7719cac
Giulio Cervera <giulio.cervera@gmail.com>
2012-02-23 19:27:47 +0100

    qcom/media: build only legacy audio
    mm-core and mm-video are 2 separate repos on ics.
    libstagefrighthw is built from hardware/msm7k and is keep
    there only for QComOMXMetadata.h inclusion.
    
    Change-Id: I0210bd41ac7c9f4c8969dca52f541af2f5759cdc

--- libstagefrighthw/Android.mk
+++
@@ -1,9 +0,0 @@
-SF_HW_ROOT := $(call my-dir)
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-    include $(SF_HW_ROOT)/msm7x30/Android.mk
-endif
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    include $(SF_HW_ROOT)/msm8x60/Android.mk
-endif
