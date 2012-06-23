273b3e05d91d97374d608bdf5ac7c2092237c0d3
Shalaj Jain <shalajj@codeaurora.org>
2012-06-22 19:08:03 -0700

    hardware/qcom/media: Initial commit
    - Includes modules : libstagefrighthw, mm-core, mm-video
    - libstagefrighthw: Adds OMX plugins for QCom component
    - mm-core: Registers QCom component
    - mm-video: Decoder and encoder component
    
    Change-Id: I3ba599ac1c8652dc5fc2f3825f6e6ab8047be347

---
+++ Android.mk
@@ -0,0 +1,5 @@
+QCOM_MEDIA_ROOT := $(call my-dir)
+
+include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
