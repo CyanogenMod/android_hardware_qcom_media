921b2e9e64cd29153e32a3a99d61d9bfdb58514c
Giulio Cervera <giulio.cervera@gmail.com>
2012-07-09 01:31:11 +0200

    audio/msm8660: Add support for sony camcorder mic
    - Sony Xperia S have camcorder_tx but it point to the primary mic
      which is placed on the bottomo of the phone, we can use
      secondary mic which is placed behind camera
    
    Change-Id: I7892495b5fc201d527617ad0a686d1e886b49b99

--- audio/msm8660/AudioHardware.cpp
+++ audio/msm8660/AudioHardware.cpp
@@ -150,7 +150,11 @@ static const uint32_t DEVICE_SPEAKER_CALL_RX       = 62; // speaker_call_rx
 static const uint32_t DEVICE_SPEAKER_CALL_TX       = 63; // speaker_call_tx
 static const uint32_t DEVICE_HEADSET_CALL_RX       = 64; // headset_call_rx
 static const uint32_t DEVICE_HEADSET_CALL_TX       = 65; // headset_call_tx
+static const uint32_t DEVICE_SPEAKER_VR_TX         = 82; // speaker_vr_tx
+static const uint32_t DEVICE_HEADSET_VR_TX         = 83; // headset_vr_tx
 #endif
+static const uint32_t DEVICE_CAMCORDER_TX          = 105; // camcoder_tx (misspelled by Samsung)
+                                                          // secondary_mic_tx (sony)
 
 static uint32_t FLUENCE_MODE_ENDFIRE   = 0;
 static uint32_t FLUENCE_MODE_BROADSIDE = 1;
@@ -811,7 +815,18 @@ AudioHardware::AudioHardware() :
             index = DEVICE_HEADSET_CALL_RX;
         else if(strcmp((char* )name[i], "headset_call_tx") == 0)
             index = DEVICE_HEADSET_CALL_TX;
+        else if(strcmp((char* )name[i], "speaker_vr_tx") == 0)
+            index = DEVICE_SPEAKER_VR_TX;
+        else if(strcmp((char* )name[i], "headset_vr_tx") == 0)
+            index = DEVICE_HEADSET_VR_TX;
 #endif
+        else if((strcmp((char* )name[i], "camcoder_tx") == 0) ||
+#ifdef SONY_AUDIO
+                (strcmp((char* )name[i], "secondary_mic_tx") == 0))
+#else
+                (strcmp((char* )name[i], "camcorder_tx") == 0))
+#endif
+            index = DEVICE_CAMCORDER_TX;
         else
             continue;
         LOGV("index = %d",index);
