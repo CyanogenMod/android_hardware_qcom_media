b057f8966a5308b0333347cf5e727f0b19fc4309
Praneeth Paladugu <ppaladug@codeaurora.org>
2012-07-10 08:09:25 -0700

    mm-video: vidc: Adds support for dynamic I frame request
    Adds support for on demand I frame request
    
    Change-Id: Iab287197b1c5512eb96f70b2c3ec68706a06c77a

--- mm-video/vidc/venc/src/video_encoder_device_msm8974.cpp
+++ mm-video/vidc/venc/src/video_encoder_device_msm8974.cpp
@@ -2588,6 +2588,18 @@ bool venc_dev::venc_set_intra_vop_refresh(OMX_BOOL intra_vop_refresh)
   DEBUG_PRINT_LOW("\n venc_set_intra_vop_refresh: intra_vop = %uc", intra_vop_refresh);
   if(intra_vop_refresh == OMX_TRUE)
   {
+	struct v4l2_control control;
+	int rc;
+	control.id = V4L2_CID_MPEG_VIDC_VIDEO_REQUEST_IFRAME;
+	control.value = 1;
+	printf("Calling IOCTL set control for id=%x, val=%d\n", control.id, control.value);
+	rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+	if (rc) {
+		printf("Failed to set Intra Frame Request control\n");
+		return false;
+	}
+	printf("Success IOCTL set control for id=%x, value=%d\n", control.id, control.value);
+
     if(/*ioctl(m_nDriver_fd, VEN_IOCTL_CMD_REQUEST_IFRAME, NULL) < */0)
     {
       DEBUG_PRINT_ERROR("\nERROR: Request for setting Intra VOP Refresh failed");
