48214433d3367d27fe3265f10ac812ffefc970ea
Sudhir Sharma <sudsha@codeaurora.org>
2012-07-10 21:57:44 -0700

    Revert "msm7627a: disable video encoder for JB bringup"
    This reverts commit c199b360adba60e15b3fbddc86a7fe4070aa00f9.

--- mm-video/Android.mk
+++ mm-video/Android.mk
@@ -1,7 +1,6 @@
 OMX_VIDEO_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-ifneq ($(TARGET_BOARD_PLATFORM),msm7627a)
+
 include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
 include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
 include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
-endif
