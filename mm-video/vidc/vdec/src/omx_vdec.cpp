bcc62c0cd17d577aee91b75f5a2dfdaedddb119c
Eric (Quic <yinlokh@codeaurora.org>
2012-08-30 15:06:10 -0700

    mm-video: vidc: Add support for Smooth streaming for secure mode playback.
        -When secure playback enabled, extra data parsing can't be done to
          find new resolution.
    
        -This change will add the extension index required for
          Smooth Streaming for both secure and non secure mode video playback.
    
    Change-Id: Ifbab34b3f56a7054a62588bacd618c0f68eddc5b
    CRs-Fixed: 368333

--- mm-video/vidc/vdec/src/omx_vdec.cpp
+++ mm-video/vidc/vdec/src/omx_vdec.cpp
@@ -3527,6 +3527,18 @@ OMX_ERRORTYPE  omx_vdec::set_parameter(OMX_IN OMX_HANDLETYPE     hComp,
          }
        }
       break;
+
+    case OMX_QcomIndexParamEnableSmoothStreaming:
+      {
+
+        int rc = ioctl(drv_ctx.video_driver_fd,
+                      VDEC_IOCTL_SET_CONT_ON_RECONFIG);
+        if(rc < 0) {
+            DEBUG_PRINT_ERROR("Failed to enable Smooth Streaming on driver.");
+            eRet = OMX_ErrorHardware;
+        }
+      }
+     break;
 #endif
 #if defined (_ANDROID_HONEYCOMB_) || defined (_ANDROID_ICS_)
       /* Need to allow following two set_parameters even in Idle
