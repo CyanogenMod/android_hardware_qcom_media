0c3c2487edf90dcc6321d36c4898feafb78a9dd3
Arun Menon <menon@codeaurora.org>
2012-10-08 13:52:44 -0700

    mm-video: set ION_SECURE to flags instead of heap_mask during ion alloc.
    Without this change secure playback will fail.
    
    Change-Id: I13dc44113a6752a9198346725e9b5155414446d5

--- mm-video/vidc/vdec/src/omx_vdec.cpp
+++ mm-video/vidc/vdec/src/omx_vdec.cpp
@@ -7521,7 +7521,8 @@ int omx_vdec::alloc_map_ion_memory(OMX_U32 buffer_size,
   }
 
   if(secure_mode) {
-    alloc_data->heap_mask = (ION_HEAP(MEM_HEAP_ID) | ION_SECURE);
+    alloc_data->heap_mask = ION_HEAP(MEM_HEAP_ID);
+    alloc_data->flags |= ION_SECURE;
   } else {
     alloc_data->heap_mask = (ION_HEAP(ION_IOMMU_HEAP_ID));
   }
