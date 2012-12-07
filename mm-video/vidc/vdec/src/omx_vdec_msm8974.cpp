14c261349c22021f595dfa6f06a85d59014fe3c5
Vinay Kalia <vkalia@codeaurora.org>
2012-12-07 15:18:14 -0800

    mm-video: vdec: Fallback to IOMMU heap
    Video buffer allocation falls back to IOMMU heap
    if MM heap runs out of memory. This helps
    reduce MM carveout size while supporting higher
    resolution encoding/decoding.
    
    CRs-fixed: 420585
    Change-Id: Iaad8efab61f8ec66ae9ada060a5e2da66d490bb3

--- mm-video/vidc/vdec/src/omx_vdec_msm8974.cpp
+++ mm-video/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -7579,6 +7579,8 @@ int omx_vdec::alloc_map_ion_memory(OMX_U32 buffer_size,
       alloc_data->flags |= ION_SECURE;
 
   alloc_data->heap_mask = ION_HEAP(MEM_HEAP_ID);
+  if (!secure_mode)
+	  alloc_data->heap_mask |= ION_HEAP(ION_IOMMU_HEAP_ID);
   rc = ioctl(fd,ION_IOC_ALLOC,alloc_data);
   if (rc || !alloc_data->handle) {
     DEBUG_PRINT_ERROR("\n ION ALLOC memory failed ");
