d62adb32035364773e491f2e5e630330a6da1304
vivek mehta <mvivek@codeaurora.org>
2012-12-11 21:56:03 -0800

    libI420colorconvert: remove libI420colourconvert
    - move this to qcom proprietay
    
    Change-Id: I51ca9f1277e069097132dbc4f6043bb4099d6115

--- Android.mk
+++ Android.mk
@@ -6,11 +6,6 @@ endif
 
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
-include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
-endif
-
-ifneq ($(filter msm7627a,$(TARGET_BOARD_PLATFORM)),)
-include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif
 
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
