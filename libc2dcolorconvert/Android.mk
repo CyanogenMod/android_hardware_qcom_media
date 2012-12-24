c22d97c5cb2ef3e8c9154de5448b9ed572c5e1e2
Steve Kondik <shade@chemlab.org>
2012-12-23 19:03:56 -0800

    media: Compensate for alternate display variant
     * Set correct path if CAF display driver is in use
    
    Change-Id: I15e172029ab9bc8df0258a4e22e365a16c8fc263

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -2,13 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
+DISPLAY := display-caf
+else
+DISPLAY := display
+endif
+
 LOCAL_SRC_FILES := \
         C2DColorConverter.cpp
 
 LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/av/include/media/stagefright \
     $(TOP)/frameworks/native/include/media/openmax \
-    $(TOP)/hardware/qcom/display/libcopybit
+    $(TOP)/hardware/qcom/$(DISPLAY)/libcopybit
 
 LOCAL_SHARED_LIBRARIES := liblog libdl
 
