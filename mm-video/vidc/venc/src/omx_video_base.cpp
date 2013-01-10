OpenSEMC/android_hardware_qcom_media-legacy

mm-video:venc: Add required ION Heap ID for video encoder buffers
- Allocate video encoder buffers from ION_CAMERA_HEAP_ID memory
  for 8x55 target.

Change-Id: If9d4d4d484d53bffe41940a04adf589d7d1a9ebe

mm-video/vidc/venc/src/omx_video_base.cpp
@@ -4293,9 +4293,12 @@ int omx_video::alloc_map_ion_memory(int size,struct ion_allocation_data *alloc_d
         if (secure_session)
            alloc_data->heap_mask = (ION_HEAP(MEM_HEAP_ID) | ION_SECURE);
         else
+#ifdef MAX_RES_720P
+           alloc_data->heap_mask = ION_HEAP(MEM_HEAP_ID);
+#else
            alloc_data->heap_mask = (ION_HEAP(MEM_HEAP_ID) |
                 ION_HEAP(ION_IOMMU_HEAP_ID));
-
+#endif
         rc = ioctl(ion_device_fd,ION_IOC_ALLOC,alloc_data);
         if(rc || !alloc_data->handle) {
            DEBUG_PRINT_ERROR("\n ION ALLOC memory failed ");

mm-video/vidc/venc/src/video_encoder_device.cpp
@@ -1252,9 +1252,13 @@ OMX_U32 venc_dev::pmem_allocate(OMX_U32 size, OMX_U32 alignment, OMX_U32 count)
 
   recon_buff[count].alloc_data.flags = 0;
   recon_buff[count].alloc_data.len = size;
+#ifdef MAX_RES_720P
+  recon_buff[count].alloc_data.heap_mask = ION_HEAP(MEM_HEAP_ID);
+#else
   recon_buff[count].alloc_data.heap_mask = (ION_HEAP(MEM_HEAP_ID) |
                   (venc_encoder->is_secure_session() ? ION_SECURE
                    : ION_HEAP(ION_IOMMU_HEAP_ID)));
+#endif
   recon_buff[count].alloc_data.align = clip2(alignment);
   if (recon_buff[count].alloc_data.align != 8192)
     recon_buff[count].alloc_data.align = 8192;

CyanogenMod/android_hardware_qcom_media

mm-video/vidc/venc/src/omx_video_base.cpp
@@ -4292,9 +4292,12 @@ int omx_video::alloc_map_ion_memory(int size,struct ion_allocation_data *alloc_d
         if (secure_session)
            alloc_data->flags = (ION_HEAP(MEM_HEAP_ID) | ION_SECURE);
         else
+#ifdef MAX_RES_720P
+           alloc_data->flags = ION_HEAP(MEM_HEAP_ID);
+#else
            alloc_data->flags = (ION_HEAP(MEM_HEAP_ID) |
                 ION_HEAP(ION_IOMMU_HEAP_ID));
-
+#endif
         rc = ioctl(ion_device_fd,ION_IOC_ALLOC,alloc_data);
         if(rc || !alloc_data->handle) {
            DEBUG_PRINT_ERROR("\n ION ALLOC memory failed ");

mm-video/vidc/venc/src/video_encoder_device.cpp
@@ -1251,9 +1251,13 @@ OMX_U32 venc_dev::pmem_allocate(OMX_U32 size, OMX_U32 alignment, OMX_U32 count)
   }
 
   recon_buff[count].alloc_data.len = size;
+#ifdef MAX_RES_720P
+  recon_buff[count].alloc_data.flags = ION_HEAP(MEM_HEAP_ID);
+#else
   recon_buff[count].alloc_data.flags = (ION_HEAP(MEM_HEAP_ID) |
                   (venc_encoder->is_secure_session() ? ION_SECURE
                    : ION_HEAP(ION_IOMMU_HEAP_ID)));
+#endif
   recon_buff[count].alloc_data.align = clip2(alignment);
   if (recon_buff[count].alloc_data.align != 8192)
     recon_buff[count].alloc_data.align = 8192;