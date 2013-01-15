effc3a74973d37ce0a0a08968660b7c976578f15
Ajit Khare <ajitk@codeaurora.org>
2013-01-14 16:19:10 -0800

    qcmediaplayer: Add qcmediaplayer for supporting DASH
    Change-Id: I3431b91116504f22a1f53743983657954e80b43c

--- Android.mk
+++ Android.mk
@@ -11,3 +11,7 @@ endif
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
+
+ifneq ($(filter msm8974 msm8960,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
+endif
