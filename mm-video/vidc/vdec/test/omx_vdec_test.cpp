008ce87d19b5086cf32afcfd3a38b293f6ea1719
Praneeth Paladugu <ppaladug@codeaurora.org>
2013-01-27 19:10:31 -0800

    mm-video : vidc: Fix an issue in vdec testapp
    Vdec test app doesn't support reconfig due to change
    in crop info. Add a check to bypass the same.
    
    Change-Id: If0ce729a7f68845f64934a42ece6f5f7e8788986
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- mm-video/vidc/vdec/test/omx_vdec_test.cpp
+++ mm-video/vidc/vdec/test/omx_vdec_test.cpp
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010-2012, Code Aurora Forum. All rights reserved.
+Copyright (c) 2010 - 2013, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -8,7 +8,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of Code Aurora nor
+    * Neither the name of The Linux Foundation nor
       the names of its contributors may be used to endorse or promote
       products derived from this software without specific prior written
       permission.
@@ -1150,6 +1150,10 @@ OMX_ERRORTYPE EventHandler(OMX_IN OMX_HANDLETYPE hComponent,
             break;
         case OMX_EventPortSettingsChanged:
             DEBUG_PRINT("OMX_EventPortSettingsChanged port[%d]\n", nData1);
+#ifdef _MSM8974_
+            if (nData2 != OMX_IndexParamPortDefinition)
+              break;
+#endif
             currentStatus = PORT_SETTING_CHANGE_STATE;
             if (waitForPortSettingsChanged)
             {
