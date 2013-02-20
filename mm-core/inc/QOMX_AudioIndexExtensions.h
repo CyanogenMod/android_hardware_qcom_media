5512d95882438ac9cf55822ed63954a6b025dc7c
Amal Paul <amal@codeaurora.org>
2013-02-19 20:23:03 -0800

    mm-core: Add support to configure ISO or ARIB coefficients
    - Add QCOM OMX extension to support configuration of ISO or
      ARIB stereo mixing coefficients.
    
    Change-Id: I6a969421184ccbe1af644ac4ed2395a8bb386d38

--- mm-core/inc/QOMX_AudioIndexExtensions.h
+++ mm-core/inc/QOMX_AudioIndexExtensions.h
@@ -63,12 +63,13 @@ extern "C"
  */
 typedef enum QOMX_AUDIO_EXTENSIONS_INDEXTYPE
 {
-    QOMX_IndexParamAudioAmrWbPlus       = 0x7F200000, /**< "OMX.Qualcomm.index.audio.amrwbplus" */
-    QOMX_IndexParamAudioWma10Pro        = 0x7F200001, /**< "OMX.Qualcomm.index.audio.wma10pro" */
-    QOMX_IndexParamAudioSessionId       = 0x7F200002, /**< "OMX.Qualcomm.index.audio.sessionId" */
-    QOMX_IndexParamAudioVoiceRecord     = 0x7F200003, /**< "OMX.Qualcomm.index.audio.VoiceRecord" */
-	QOMX_IndexConfigAudioDualMono       = 0x7F200004, /**< "OMX.Qualcomm.index.audio.dualmono" */
-    QOMX_IndexParamAudioUnused          = 0x7F2FFFFF
+    QOMX_IndexParamAudioAmrWbPlus        = 0x7F200000, /**< "OMX.Qualcomm.index.audio.amrwbplus" */
+    QOMX_IndexParamAudioWma10Pro         = 0x7F200001, /**< "OMX.Qualcomm.index.audio.wma10pro" */
+    QOMX_IndexParamAudioSessionId        = 0x7F200002, /**< "OMX.Qualcomm.index.audio.sessionId" */
+    QOMX_IndexParamAudioVoiceRecord      = 0x7F200003, /**< "OMX.Qualcomm.index.audio.VoiceRecord" */
+    QOMX_IndexConfigAudioDualMono        = 0x7F200004, /**< "OMX.Qualcomm.index.audio.dualmono" */
+    QOMX_IndexParamAudioAacSelectMixCoef = 0x7F200005, /**< "OMX.Qualcomm.index.audio.aac_sel_mix_coef" */
+    QOMX_IndexParamAudioUnused           = 0x7F2FFFFF
 } QOMX_AUDIO_EXTENSIONS_INDEXTYPE;
 
 #if defined( __cplusplus )
