9f7378cb09aeb1c6681a3a1ac7a284e86d4e436c
Adam Farden <adam@farden.cz>
2013-04-02 16:19:28 +0200

    Allow alternative QCOM media to be used
    Most devices not upgraded to Android 4.1 will use the adreno blobs
    found on the Qualcomm website. Some devices that are upgraded
    to or come with Android 4.1 have newer adreno blobs, which perform
    significantly better. However (at least in the case of Sony Xperia J)
    this repo is not compatible with those newer blobs.
    
    This follows the CM10.1 pattern to allow an alternative display
    repo to be used.
    
    Change-Id: I0b526028d9fc9842d3bbb6f5ce3844fe81de6c50

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -7,4 +8,5 @@ endif
 ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+endif
 endif
