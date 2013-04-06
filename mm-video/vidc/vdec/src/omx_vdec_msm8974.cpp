212671fbaef18bc8e5b1881783643618ab7024a4
Praveen Chavan <pchavan@codeaurora.org>
2013-04-05 20:00:42 -0700

    mm-video: vidc: do not unmap buffer handles in secure mode
    In secure mode, output buffer handles are stowed with gralloc
    handles and do not have mapped pointers. In free, this should
    not be unmapped.
    For input case too the ion fds should not be unmapped while
    freeing in secure mode.
    
    Change-Id: I0ea8d19d8dc7bd0e5c51a06d32497891476e375f
    CRs-Fixed: 465905

--- mm-video/vidc/vdec/src/omx_vdec_msm8974.cpp
+++ mm-video/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -4518,13 +4518,15 @@ OMX_ERRORTYPE omx_vdec::free_input_buffer(OMX_BUFFERHEADERTYPE *bufferHdr)
        setbuffers.buffer_type = VDEC_BUFFER_TYPE_INPUT;
        memcpy (&setbuffers.buffer,&drv_ctx.ptr_inputbuffer[index],
           sizeof (vdec_bufferpayload));
-       DEBUG_PRINT_LOW("\n unmap the input buffer fd=%d",
-                    drv_ctx.ptr_inputbuffer[index].pmem_fd);
-       DEBUG_PRINT_LOW("\n unmap the input buffer size=%d  address = %p",
-                    drv_ctx.ptr_inputbuffer[index].mmaped_size,
-                    drv_ctx.ptr_inputbuffer[index].bufferaddr);
-       munmap (drv_ctx.ptr_inputbuffer[index].bufferaddr,
-               drv_ctx.ptr_inputbuffer[index].mmaped_size);
+       if(!secure_mode) {
+           DEBUG_PRINT_LOW("\n unmap the input buffer fd=%d",
+                        drv_ctx.ptr_inputbuffer[index].pmem_fd);
+           DEBUG_PRINT_LOW("\n unmap the input buffer size=%d  address = %p",
+                       drv_ctx.ptr_inputbuffer[index].mmaped_size,
+                       drv_ctx.ptr_inputbuffer[index].bufferaddr);
+           munmap (drv_ctx.ptr_inputbuffer[index].bufferaddr,
+                   drv_ctx.ptr_inputbuffer[index].mmaped_size);
+       }
        close (drv_ctx.ptr_inputbuffer[index].pmem_fd);
        drv_ctx.ptr_inputbuffer[index].pmem_fd = -1;
        if (m_desc_buffer_ptr && m_desc_buffer_ptr[index].buf_addr)
@@ -4566,26 +4568,30 @@ OMX_ERRORTYPE omx_vdec::free_output_buffer(OMX_BUFFERHEADERTYPE *bufferHdr)
         sizeof (vdec_bufferpayload));
 #ifdef _ANDROID_
     if(m_enable_android_native_buffers) {
-        if(drv_ctx.ptr_outputbuffer[index].pmem_fd > 0) {
-            munmap(drv_ctx.ptr_outputbuffer[index].bufferaddr,
-                    drv_ctx.ptr_outputbuffer[index].mmaped_size);
+            if (!secure_mode) {
+            if(drv_ctx.ptr_outputbuffer[index].pmem_fd > 0) {
+                munmap(drv_ctx.ptr_outputbuffer[index].bufferaddr,
+                        drv_ctx.ptr_outputbuffer[index].mmaped_size);
+            }
         }
         drv_ctx.ptr_outputbuffer[index].pmem_fd = -1;
     } else {
 #endif
         if (drv_ctx.ptr_outputbuffer[0].pmem_fd > 0 && !ouput_egl_buffers && !m_use_output_pmem)
         {
-            DEBUG_PRINT_LOW("\n unmap the output buffer fd = %d",
-                    drv_ctx.ptr_outputbuffer[0].pmem_fd);
-            DEBUG_PRINT_LOW("\n unmap the ouput buffer size=%d  address = %p",
-                    drv_ctx.ptr_outputbuffer[0].mmaped_size * drv_ctx.op_buf.actualcount,
-                    drv_ctx.ptr_outputbuffer[0].bufferaddr);
-            munmap (drv_ctx.ptr_outputbuffer[0].bufferaddr,
-                    drv_ctx.ptr_outputbuffer[0].mmaped_size * drv_ctx.op_buf.actualcount);
-          close (drv_ctx.ptr_outputbuffer[0].pmem_fd);
-          drv_ctx.ptr_outputbuffer[0].pmem_fd = -1;
+            if (!secure_mode) {
+                DEBUG_PRINT_LOW("\n unmap the output buffer fd = %d",
+                        drv_ctx.ptr_outputbuffer[0].pmem_fd);
+                DEBUG_PRINT_LOW("\n unmap the ouput buffer size=%d  address = %p",
+                        drv_ctx.ptr_outputbuffer[0].mmaped_size * drv_ctx.op_buf.actualcount,
+                        drv_ctx.ptr_outputbuffer[0].bufferaddr);
+                munmap (drv_ctx.ptr_outputbuffer[0].bufferaddr,
+                        drv_ctx.ptr_outputbuffer[0].mmaped_size * drv_ctx.op_buf.actualcount);
+            }
+            close (drv_ctx.ptr_outputbuffer[0].pmem_fd);
+            drv_ctx.ptr_outputbuffer[0].pmem_fd = -1;
 #ifdef USE_ION
-       free_ion_memory(&drv_ctx.op_buf_ion_info[0]);
+            free_ion_memory(&drv_ctx.op_buf_ion_info[0]);
 #endif
         }
 #ifdef _ANDROID_
