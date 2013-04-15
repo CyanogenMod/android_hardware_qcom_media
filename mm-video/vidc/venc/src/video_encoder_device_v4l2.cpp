38ff6e928e623dd6a39ad1ecd6501de6f742b1ba
Praneeth Paladugu <ppaladug@codeaurora.org>
2013-04-15 01:04:02 -0700

    mm-video: vidc: Inititalize extrata_info strutcure
    Initialize extradata_info structure for encoder component
    before it is being accessed.
    
    Change-Id: I3447b1a932840ec6e1dc86646c83544e8731eb49

--- mm-video/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -179,6 +179,7 @@ venc_dev::venc_dev(class omx_venc *venc_class)
 	paused = false;
 	pthread_mutex_init(&pause_resume_mlock, NULL);
 	pthread_cond_init(&pause_resume_cond, NULL);
+	memset(&extradata_info, 0, sizeof(extradata_info));
 }
 
 venc_dev::~venc_dev()
