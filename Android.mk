fc1546815c040df49e7f7a695dc6d49fc9d63811
Steve Kondik <shade@chemlab.org>
2013-06-18 20:28:59 -0700

    media: Only build when no variant is set
    Change-Id: I4363e0c9163050932a52711d080b0957e1d5925c

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),)
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -14,4 +16,6 @@ endif
 
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+endif
+
 endif
