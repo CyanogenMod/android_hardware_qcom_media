9b1d0f2152d3dba68ca00971d11f9d2c3f181831
Steve Kondik <shade@chemlab.org>
2013-06-18 20:28:59 -0700

    media: Only build when no variant is set
    Change-Id: I4363e0c9163050932a52711d080b0957e1d5925c

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
-ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
 
 include $(call all-subdir-makefiles)
 
