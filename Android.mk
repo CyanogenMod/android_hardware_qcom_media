aaad4616a3cecd4f631d1d167fc97b3cb65f66d8
Chirayu Desai <cdesai@cyanogenmod.org>
2013-06-20 09:38:20 +0530

    Only build when no media variant is set, and display variant isn't legacy
    * Fixes change I4363e0c9163050932a52711d080b0957e1d5925c
    
    Change-Id: Id7e5f5f5019fec5cce169d7ff5fe7d3c707562eb

--- Android.mk
+++ Android.mk
@@ -1,7 +1,9 @@
 ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
+ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),)
 
 include $(call all-subdir-makefiles)
 
+endif
 endif
 endif
