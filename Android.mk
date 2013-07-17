a4bba368421a1982096276c660be9b3d4b437659
AUDITYA BHATTARAM <audityab@codeaurora.org>
2013-07-17 18:59:21 +0530

    Enable qcmediaplayer to compile for msm8610 target.
    Enable qcmediaplayer to compile for msm8610 target.
    
    Change-Id: Ibeb69d06b8987478b5ac75e4b1ed59e7abf83780

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@ ifneq ($(filter msm8610 msm8226 msm8974 msm8960 apq8084 mpq8092,$(TARGET_BOARD_P
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 
-ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092 msm8610 ,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
 endif
