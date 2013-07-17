7c6d2992a7e72181eb4bc210f040e4767e815d26
Praneeth Paladugu <ppaladug@codeaurora.org>
2013-07-16 17:28:05 -0700

    mm-video: vidc: Add support for encoder scaling
    Venus FW supports upscaling and downscaling. The supported
    scaling ratios are returned as part of session_init_done
    message. This change adds the support for encoder scaling.
    
    Change-Id: Ia27db8dfc94fc0a226aa3cf73e73dd462bdd42b2

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -1431,7 +1431,11 @@ OMX_ERRORTYPE  omx_video::get_parameter(OMX_IN OMX_HANDLETYPE     hComp,
 
                 DEBUG_PRINT_LOW("get_parameter: OMX_IndexParamPortDefinition\n");
                 if (portDefn->nPortIndex == (OMX_U32) PORT_INDEX_IN) {
-                    DEBUG_PRINT_LOW("m_sInPortDef: size = %d, min cnt = %d, actual cnt = %d",
+                    dev_get_buf_req(&m_sInPortDef.nBufferCountMin,
+                        &m_sInPortDef.nBufferCountActual,
+                        &m_sInPortDef.nBufferSize,
+                        m_sInPortDef.nPortIndex);
+                    DEBUG_PRINT_LOW("m_sInPortDef: size = %lu, min cnt = %lu, actual cnt = %lu",
                             m_sInPortDef.nBufferSize, m_sInPortDef.nBufferCountMin,
                             m_sInPortDef.nBufferCountActual);
                     memcpy(portDefn, &m_sInPortDef, sizeof(m_sInPortDef));
