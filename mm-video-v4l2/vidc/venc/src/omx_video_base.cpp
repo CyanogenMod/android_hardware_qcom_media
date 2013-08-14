0b6fd256ee76a3fbbfcd4a33c63be88b1db78798
Praveen Chavan <pchavan@codeaurora.org>
2013-08-13 20:21:31 -0700

    mm-video: venc: Distinguish between normal NV12 and Venus's NV12
    Previously we were advertising OMX_COLOR_FormatYUV420SemiPlanar as the
    NV12 colorformat "natively" supported by the component. However, this is
    taken to mean the vanilla version of NV12 by many clients (e.g. Google).
    As a result, whenever we thought that a non-camera client was providing
    NV12, we'd align to Venus' liking internally. This doesn't work entirely
    because there are other apps (e.g VT) that are aware that the component
    wants aligned NV12 and would align it themselves, causing the component
    to muck up the buffer by realigning it.
    
    To prevent the confusion, the component now makes a distinction between
    NV12 (OMX_COLOR_FormatYUV420SemiPlanar) and Venus' NV12 (QOMX_COLOR_\
    FORMATYUV420PackedSemiPlanar32).  Clients who prefer to provide aligned
    buffers will now set the latter as the input port format.
    
    Change-Id: I1e618c8bbb4b2002caa0967d7bd0ff363ab72763
    Signed-off-by:  Deva Ramasubramanian <dramasub@codeaurora.org>

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -1452,20 +1452,23 @@ OMX_ERRORTYPE  omx_video::get_parameter(OMX_IN OMX_HANDLETYPE     hComp,
 
                 if (portFmt->nPortIndex == (OMX_U32) PORT_INDEX_IN) {
                     int index = portFmt->nIndex;
-                    if (index > 1)
+                    //we support two formats
+                    //index 0 - Venus flavour of YUV420SP
+                    //index 1 - opaque which internally maps to YUV420SP.
+                    //index 2 - vannilla YUV420SP
+                    //this can be extended in the future
+                    int supportedFormats[] = {
+                        [0] = QOMX_COLOR_FORMATYUV420PackedSemiPlanar32m,
+                        [1] = QOMX_COLOR_FormatAndroidOpaque,
+                        [2] = OMX_COLOR_FormatYUV420SemiPlanar,
+                    };
+
+                    if (index > sizeof(supportedFormats)/sizeof(*supportedFormats))
                         eRet = OMX_ErrorNoMore;
                     else {
                         memcpy(portFmt, &m_sInPortFormat, sizeof(m_sInPortFormat));
-#ifdef _ANDROID_ICS_
-                        if (index == 1) {
-                            //we support two formats
-                            //index 0 - YUV420SP
-                            //index 1 - opaque which internally maps to YUV420SP.
-                            //this can be extended in the future
-                            portFmt->nIndex = index; //restore index set from client
-                            portFmt->eColorFormat = (OMX_COLOR_FORMATTYPE)QOMX_COLOR_FormatAndroidOpaque;
-                        }
-#endif
+                        portFmt->nIndex = index; //restore index set from client
+                        portFmt->eColorFormat = (OMX_COLOR_FORMATTYPE)supportedFormats[index];
                     }
                 } else if (portFmt->nPortIndex == (OMX_U32) PORT_INDEX_OUT) {
                     memcpy(portFmt, &m_sOutPortFormat, sizeof(m_sOutPortFormat));
@@ -3197,7 +3200,7 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer_proxy(OMX_IN OMX_HANDLETYPE         
     } else if (mUseProxyColorFormat) {
         fd = m_pInput_pmem[nBufIndex].fd;
     } else if (m_sInPortDef.format.video.eColorFormat ==
-                    OMX_COLOR_FormatYUV420SemiPlanar && !mUseProxyColorFormat) {
+                    OMX_COLOR_FormatYUV420SemiPlanar) {
             //For the case where YUV420SP buffers are qeueued to component
             //by sources other than camera (Apps via MediaCodec), conversion
             //to vendor flavoured NV12 color format is required.
@@ -3210,18 +3213,18 @@ OMX_ERRORTYPE  omx_video::empty_this_buffer_proxy(OMX_IN OMX_HANDLETYPE         
 #ifdef _MSM8974_
     if (dev_empty_buf(buffer, pmem_data_buf,nBufIndex,fd) != true)
 #else
-        if (dev_empty_buf(buffer, pmem_data_buf,0,0) != true)
+    if (dev_empty_buf(buffer, pmem_data_buf,0,0) != true)
 #endif
-        {
-            DEBUG_PRINT_ERROR("\nERROR: ETBProxy: dev_empty_buf failed");
+    {
+        DEBUG_PRINT_ERROR("\nERROR: ETBProxy: dev_empty_buf failed");
 #ifdef _ANDROID_ICS_
-            omx_release_meta_buffer(buffer);
+        omx_release_meta_buffer(buffer);
 #endif
-            post_event ((unsigned int)buffer,0,OMX_COMPONENT_GENERATE_EBD);
-            /*Generate an async error and move to invalid state*/
-            pending_input_buffers--;
-            return OMX_ErrorBadParameter;
-        }
+        post_event ((unsigned int)buffer,0,OMX_COMPONENT_GENERATE_EBD);
+        /*Generate an async error and move to invalid state*/
+        pending_input_buffers--;
+        return OMX_ErrorBadParameter;
+    }
     return ret;
 }
 
