5c2fe34ec2753fe98b1c4f9f12fbbd80c69283a0
Anh Nguyen <anguyen@codeaurora.org>
2013-11-05 15:06:20 -0800

    dashplayer: disable to resolve compilation error
    disable dashplayer to resolve compilation error
    
    Change-Id: I576e4889aab05e87f86bf6d4672880115144bc16

--- Android.mk
+++ Android.mk
@@ -13,6 +13,8 @@ include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 
 ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092 msm8610 msm_bronze,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(call is-platform-sdk-version-at-least, 19),true)
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
+endif
 endif
