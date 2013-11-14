9bc7093889fab9d58828c240562930e16d089366
Deva Ramasubramanian <dramasub@codeaurora.org>
2013-11-13 17:52:32 -0800

    mm-core: For HEVC decoding use libOmxVdec.so
    The registry table for android and the omx test app are out of sync. The
    android registry table should use libOmxVdec for HEVC decoding as well.
    
    Change-Id: Ie4a7a9fd61f3a216fd369120a46b74aa4a5c3dcc

--- mm-core/src/8084/qc_registry_table_android.c
+++ mm-core/src/8084/qc_registry_table_android.c
@@ -210,7 +210,7 @@ omx_core_cb_type core[] =
       NULL
     },
     NULL,   // Shared object library handle
-    "libOmxVdecHevc.so",
+    "libOmxVdec.so",
     {
       "video_decoder.hevc"
     }
