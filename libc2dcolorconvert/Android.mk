c3305d9e8a12f303a863348111a12b912fa9f280
dhacker29 <davidhackerdvm@gmail.com>
2013-11-18 18:30:16 -0600

    Use actual kernel headers and display-caf paths if defined
    Change-Id: I7338867f6eb4d88b33b610a2b2f93280ed6e71bc

--- libc2dcolorconvert/Android.mk
+++ libc2dcolorconvert/Android.mk
@@ -8,7 +8,10 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/av/include/media/stagefright \
     $(TOP)/frameworks/native/include/media/openmax \
-    $(TOP)/hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libcopybit
+    $(TOP)/hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libcopybit \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := liblog libdl
 
