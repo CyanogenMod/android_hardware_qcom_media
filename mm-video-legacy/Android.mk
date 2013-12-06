f5a6dd3faa71350d39859f4fa89ba97bba3c0f99
syhost <soyudesign@gmail.com>
2013-12-07 04:57:40 +0800

    media: fix msm8660 omx issue
    for msm8660: previous code lost libOmxVdec.so & libOmxVenc.so , now fixed it!
    
    Change-Id: Ie6db1be912f0411bca47e9bfba9d0b7053febb18

--- mm-video-legacy/Android.mk
+++ mm-video-legacy/Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660,$(TARGET_BOARD_PLATFORM)),)
 OMX_VIDEO_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
--- mm-video-legacy/vidc/vdec/src/omx_vdec.cpp
+++ mm-video-legacy/vidc/vdec/src/omx_vdec.cpp
@@ -7610,7 +7610,7 @@ int omx_vdec::alloc_map_ion_memory(OMX_U32 buffer_size,
     alloc_data->heap_mask = ION_HEAP(MEM_HEAP_ID);
     alloc_data->flags |= ION_SECURE;
   } else {
-    alloc_data->heap_mask = (ION_HEAP(ION_IOMMU_HEAP_ID));
+    alloc_data->heap_mask = (ION_HEAP(MEM_HEAP_ID) | ION_HEAP(ION_IOMMU_HEAP_ID));
   }
   rc = ioctl(fd,ION_IOC_ALLOC,alloc_data);
   if (rc || !alloc_data->handle) {
