47265f00234a1e9566c5eb9af089d04f7bc5bbd9
Jorge Solano Altamirano <jsolano@codeaurora.org>
2013-12-06 14:01:46 -0800

    mm-core: 8092: Add registry table for new OMX MVC component
    Add MVC to to the list of supported codecs.
    
    Change-Id: I86a180a405210380b6137015f3735f6807f8eb52

--- mm-core/src/8092/qc_registry_table_android.c
+++ mm-core/src/8092/qc_registry_table_android.c
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2012 - 2013, The Linux Foundation. All rights reserved.
+Copyright (c) 2012 - 2014, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -69,6 +69,22 @@ omx_core_cb_type core[] =
     "libOmxVdec.so",
     {
       "video_decoder.avc"
+    }
+  },
+  {
+    "OMX.qcom.video.decoder.mvc",
+    NULL,   // Create instance function
+    // Unique instance handle
+    {
+      NULL,
+      NULL,
+      NULL,
+      NULL
+    },
+    NULL,   // Shared object library handle
+    "libOmxVdec.so",
+    {
+      "video_decoder.mvc"
     }
   },
   {
