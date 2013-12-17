ba4534ba7c26d0822db2cf48faa9b4869b8a0eb1
Deva Ramasubramanian <dramasub@codeaurora.org>
2013-12-17 15:52:37 -0800

    mm-video: Recognise "OMX.qcom.video.decoder.divx4" when setting role
    Setting the component role for the "OMX.qcom.video.decoder.divx4"
    component triggered an incorrect claiming that the component wasn't
    recognised. This is a valid component.
    
    Change-Id: Ib58ef8d74dcaaf6bde4bc4a07adca400822e0a11
    CRs-Fixed: 759443

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -3399,7 +3399,8 @@ OMX_ERRORTYPE  omx_vdec::set_parameter(OMX_IN OMX_HANDLETYPE     hComp,
                                           eRet = OMX_ErrorUnsupportedSetting;
                                       }
                                   } else if ((!strncmp(drv_ctx.kind, "OMX.qcom.video.decoder.divx",OMX_MAX_STRINGNAME_SIZE)) ||
-                                          (!strncmp(drv_ctx.kind, "OMX.qcom.video.decoder.divx311",OMX_MAX_STRINGNAME_SIZE))
+                                          (!strncmp(drv_ctx.kind, "OMX.qcom.video.decoder.divx311", OMX_MAX_STRINGNAME_SIZE)) ||
+                                          (!strncmp(drv_ctx.kind, "OMX.qcom.video.decoder.divx4", OMX_MAX_STRINGNAME_SIZE))
                                         ) {
                                       if (!strncmp((const char*)comp_role->cRole,"video_decoder.divx",OMX_MAX_STRINGNAME_SIZE)) {
                                           strlcpy((char*)m_cRole,"video_decoder.divx",OMX_MAX_STRINGNAME_SIZE);
