a2efdb16c85e7d4d180ed171851fe6f113dcf675
Deepak Verma <dverma@codeaurora.org>
2013-12-26 12:30:05 +0530

    mm-video: vdec: Add adaptive playback support for Q6
    This change is required to support adaptive playback
    on Q6 supported targets.
    
    Change-Id: I3143c81842f382cd4e61fe5b56209f51a4fa7c27

--- mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
+++ mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010 - 2013, The Linux Foundation. All rights reserved.
+Copyright (c) 2010 - 2014, The Linux Foundation. All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions
@@ -908,6 +908,7 @@ class omx_vdec: public qc_omx_component
         bool is_down_scalar_enabled;
 #endif
         bool m_power_hinted;
+        bool is_q6_platform;
         OMX_ERRORTYPE power_module_register();
         OMX_ERRORTYPE power_module_deregister();
         bool msg_thread_created;
