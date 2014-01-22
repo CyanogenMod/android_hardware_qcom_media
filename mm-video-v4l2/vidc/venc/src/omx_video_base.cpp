433361d01ee8f965e1b6091e4cbd19ce4a709242
Ashray Kulkarni <ashrayk@codeaurora.org>
2014-01-22 14:49:24 -0800

    vidc: venc: add support to configure initialqp to encoder
    Enable setting initialqp and add support to allow setting initial
    qp values to I p & B frames to encoder session.
    
    Change-Id: I9bef96199c24ac64bff1f92dd6be3f256dc73220

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -1848,6 +1848,13 @@ OMX_ERRORTYPE  omx_video::get_parameter(OMX_IN OMX_HANDLETYPE     hComp,
                     pParam->nPeakBitrate = peakbitrate;
                 }
                 break;
+        }
+        case QOMX_IndexParamVideoInitialQp:
+            {
+                 QOMX_EXTNINDEX_VIDEO_INITIALQP* initqp =
+                     reinterpret_cast<QOMX_EXTNINDEX_VIDEO_INITIALQP *>(paramData);
+                 memcpy(initqp, &m_sParamInitqp, sizeof(m_sParamInitqp));
+                 break;
             }
         case OMX_IndexParamVideoSliceFMO:
         default:
