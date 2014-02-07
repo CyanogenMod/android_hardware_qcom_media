270296aefd9b5f6996eda435c379ee4f736f105f
Praveen Chavan <pchavan@codeaurora.org>
2014-02-06 16:15:46 -0800

    mm-video: c2dColorConverter: allow 16-byte aligned stride for NV12
    Remove 32-bit restriction on stride if target format is YCbCr420SP.
    This lets conversion of non-32 aligned resolution (like QCIF) without
    padding.
    
    Change-Id: I5e42606a05dfb7ef55ea1eb1f06f581d23e123dd
    CRs-Fixed: 620886

--- libc2dcolorconvert/C2DColorConverter.cpp
+++ libc2dcolorconvert/C2DColorConverter.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012 - 2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012 - 2014, The Linux Foundation. All rights reserved.
  *
  * redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -398,7 +398,7 @@ size_t C2DColorConverter::calcStride(ColorConvertFormat format, size_t width)
         case YCbCr420Tile:
             return ALIGN(width, ALIGN128);
         case YCbCr420SP:
-            return ALIGN(width, ALIGN32);
+            return ALIGN(width, ALIGN16);
         case NV12_2K:
             return ALIGN(width, ALIGN16);
         case NV12_128m:
@@ -416,7 +416,7 @@ size_t C2DColorConverter::calcYSize(ColorConvertFormat format, size_t width, siz
 {
     switch (format) {
         case YCbCr420SP:
-            return (ALIGN(width, ALIGN32) * height);
+            return (ALIGN(width, ALIGN16) * height);
         case YCbCr420P:
             return width * height;
         case YCrCb420P:
@@ -454,7 +454,7 @@ size_t C2DColorConverter::calcSize(ColorConvertFormat format, size_t width, size
             size = ALIGN(size, ALIGN4K);
             break;
         case YCbCr420SP:
-            alignedw = ALIGN(width, ALIGN32);
+            alignedw = ALIGN(width, ALIGN16);
             size = ALIGN((alignedw * height) + (ALIGN(width/2, ALIGN32) * (height/2) * 2), ALIGN4K);
             break;
         case YCbCr420P:
