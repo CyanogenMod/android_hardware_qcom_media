5d00285eb47bfebe9ef426e0b7f86d97522bf3d6
Deva Ramasubramanian <dramasub@codeaurora.org>
2014-02-27 18:08:05 -0800

    mm-video: vidc: Fix bugs introduced via merge conflict
    Fix a couple of bugs introduced by bad conflict resolutions.  A couple
    of `if(...)` should be `else if (...)`.  This caused setting of
    EncoderMultislice extradata to always fail.
    
    Change-Id: I07aeb61b8eb53ebd5128b8d700fa87e8c2feb97b

--- mm-video-v4l2/vidc/venc/src/omx_video_encoder.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_encoder.cpp
@@ -1070,9 +1070,7 @@ OMX_ERRORTYPE  omx_venc::set_parameter(OMX_IN OMX_HANDLETYPE     hComp,
                         eRet = OMX_ErrorUnsupportedIndex;
                         break;
                     }
-                }
-
-                if (pParam->nIndex == (OMX_INDEXTYPE)OMX_ExtraDataVideoEncoderMBInfo) {
+                } else if (pParam->nIndex == (OMX_INDEXTYPE)OMX_ExtraDataVideoEncoderMBInfo) {
                     if (pParam->nPortIndex == PORT_INDEX_OUT) {
                         mask = VEN_EXTRADATA_MBINFO;
 
