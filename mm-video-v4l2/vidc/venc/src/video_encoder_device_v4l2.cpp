3398e64110872417eaa4ef1b385f6b6493a142df
Srinu Gorle <sgorle@codeaurora.org>
2014-03-26 20:59:55 +0530

    mm-video-v4l2: venc: set intra period as per client setting
    Intra period value set by the client is being ignored in the
    component. Set intra period as per the client settings.
    
    CRs-Fixed: 640410
    Change-Id: I06229e3245b4f7987eaa6d9a3daf38b5de621c34

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -3145,11 +3145,7 @@ bool venc_dev::venc_set_intra_period(OMX_U32 nPFrames, OMX_U32 nBFrames)
     }
 
     control.id = V4L2_CID_MPEG_VIDC_VIDEO_NUM_P_FRAMES;
-    if (!nBFrames) {
-        control.value = nPFrames;
-    } else {
-        control.value = (m_sVenc_cfg.fps_num /m_sVenc_cfg.fps_den) - (nBFrames+1);
-    }
+    control.value = nPFrames;
     rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
 
     if (rc) {
