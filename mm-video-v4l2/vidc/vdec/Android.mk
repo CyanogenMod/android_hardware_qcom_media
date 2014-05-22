90c307e41d2dcd0a0509537006b9b769107a9711
Praneeth Paladugu <ppaladug@codeaurora.org>
2014-05-22 16:26:55 -0700

    mm-video-v4l2: vidc: vdec: Add HEVC HW decoder support
    MSM8994 supports HEVC decoding through Venus HW. Hence
    adds the HW codec support in OMX component.
    
    Change-Id: I5425afec2080eb9a48597fe95d1ff4cf47bf2fa0

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -96,7 +96,7 @@ libOmxVdec-def += -DMAX_RES_1080P
 libOmxVdec-def += -DMAX_RES_1080P_EBI
 libOmxVdec-def += -DPROCESS_EXTRADATA_IN_OUTPUT_PORT
 libOmxVdec-def += -D_MSM8974_
-libOmxVdec-def += -D_HEVC_USE_ADSP_HEAP_
+libOmxVdec-def += -DVENUS_HEVC
 endif
 
 libOmxVdec-def += -D_ANDROID_ICS_
