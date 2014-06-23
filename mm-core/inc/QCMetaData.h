7f2609c9a1fa732e24ec0edbecab2712e3b6e1c8
Praveen Chavan <pchavan@codeaurora.org>
2014-06-23 13:27:17 -0700

    QCMetadata: add keys for FLAC meta data
    Add FLAC meta data specific keys.
    
    Change-Id: I85f34d01ad4b403e236a5fc2ebf054eb383abbde

--- mm-core/inc/QCMetaData.h
+++ mm-core/inc/QCMetaData.h
@@ -65,6 +65,13 @@ enum {
     kKeyUseArbitraryMode     = 'ArbM',  //bool (int32_t)
     kKeySmoothStreaming      = 'ESmS',  //bool (int32_t)
     kKeyHFR                  = 'hfr ',  // int32_t
+
+    kKeySampleBits        = 'sbit', // int32_t (audio sample bit-width)
+    kKeyMinBlkSize        = 'mibs', //int32_t
+    kKeyMaxBlkSize        = 'mabs', //int32_t
+    kKeyMinFrmSize        = 'mifs', //int32_t
+    kKeyMaxFrmSize        = 'mafs', //int32_t
+    kKeyMd5Sum            = 'md5s', //cstring
 };
 
 #if 0
