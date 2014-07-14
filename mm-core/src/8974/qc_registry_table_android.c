6b7829fbc6dc6a5c3b1191123704cf6295e0f991
Steve Kondik <shade@chemlab.org>
2014-07-13 17:43:48 -0700

    mm-core: Kill logspam
    Change-Id: I329ca5499a6d2bd75b8304e6c9cbcd8763a4b0d6

--- mm-core/src/8974/qc_registry_table_android.c
+++ mm-core/src/8974/qc_registry_table_android.c
@@ -391,19 +391,6 @@ omx_core_cb_type core[] =
     }
   },
   {
-    "OMX.qcom.audio.decoder.aac",
-    NULL,   // Create instance function
-    // Unique instance handle
-    {
-      NULL
-    },
-    NULL,   // Shared object library handle
-    "libOmxAacDec.so",
-    {
-      "audio_decoder.aac"
-    }
-  },
-  {
     "OMX.qcom.audio.encoder.aac",
     NULL, // Create instance function
     // Unique instance handle
