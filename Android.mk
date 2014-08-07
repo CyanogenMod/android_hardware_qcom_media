fd43856ddb498bd0b86ffa647a99410df8839680
Sridhar Gujje <sgujje@codeaurora.org>
2014-08-07 05:17:41 -0700

    Revert "Compilation fix" .
    This reverts commit 23f6de88990cfbcc66bcab1b3b15853cd841d701
    
    Change-Id: I92c0ff3d9233f316cbde70a95313646cf4db0c22

--- Android.mk
+++ Android.mk
@@ -13,6 +13,6 @@ include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 
 ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092 msm8610 ,$(TARGET_BOARD_PLATFORM)),)
-#include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
-#include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
+include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
+include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
 endif
