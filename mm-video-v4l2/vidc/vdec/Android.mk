8ff08908964108d9d1ea96181ebb6fd9bff750dd
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2014-08-11 15:25:02 +0530

    mm-video-v4l2: vidc: vdec: Enable HEVC compilation
    Enable compilation of software HEVC
    
    Change-Id: I6f650b5ec595589ae6629d3d508869cde07f63f8

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -193,7 +193,7 @@ LOCAL_SRC_FILES         += ../common/src/extra_data_handler.cpp
 LOCAL_SRC_FILES         += ../common/src/vidc_color_converter.cpp
 LOCAL_ADDITIONAL_DEPENDENCIES  := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-#include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
 
 endif
 endif
