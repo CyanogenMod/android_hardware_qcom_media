5318f0eff9c7ec9e816c68826b4f0e97b0792500
Arun Menon <avmenon@codeaurora.org>
2014-09-11 15:00:26 -0700

    mm-video-v4l2: vdec: fix crash in omx component
    If driver is configured in dynamic buffer mode as default,
    then we see a crash in omx component. When OMX Component is
    not in dynamic buffer mode and driver sends the release reference
    and fbd events, component tries to update the out_dynamic_list
    which is NULL, resulting in a crash.
    
    Change-Id: I926ed50857c50cdcb1909696d04c51c01700f403

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -10214,6 +10214,16 @@ void omx_vdec::buf_ref_add(OMX_U32 fd, OMX_U32 offset)
 {
     unsigned long i = 0;
     bool buf_present = false;
+
+    if (!dynamic_buf_mode) {
+        return;
+    }
+
+    if (!out_dynamic_list) {
+        DEBUG_PRINT_ERROR("buf_ref_add: out_dynamic_list is NULL");
+        return;
+    }
+
     pthread_mutex_lock(&m_lock);
     for (i = 0; i < drv_ctx.op_buf.actualcount; i++) {
         //check the buffer fd, offset, uv addr with list contents
@@ -10247,6 +10257,16 @@ void omx_vdec::buf_ref_add(OMX_U32 fd, OMX_U32 offset)
 void omx_vdec::buf_ref_remove(OMX_U32 fd, OMX_U32 offset)
 {
     unsigned long i = 0;
+
+    if (!dynamic_buf_mode) {
+        return;
+    }
+
+    if (!out_dynamic_list) {
+        DEBUG_PRINT_ERROR("buf_ref_add: out_dynamic_list is NULL");
+        return;
+    }
+
     pthread_mutex_lock(&m_lock);
     for (i = 0; i < drv_ctx.op_buf.actualcount; i++) {
         //check the buffer fd, offset, uv addr with list contents
