a4e6ec3f02324cd33337555f25fe86f8c0d6f7bf
Ethan Chen <intervigil@gmail.com>
2014-11-08 23:34:23 -0800

    media: Use project pathmap
    Change-Id: I0fd4523167be90302b5db284ccf88ff79d7965dd

--- Android.mk
+++ Android.mk
@@ -1,5 +1,9 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-media)/$(TARGET_BOARD_PLATFORM))
+
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
 
 include $(call all-subdir-makefiles)
+
+endif
 
 endif
