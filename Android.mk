96a37a7cf410c6fb4244ff8618e3d8b2267047a0
Ethan Chen <intervigil@gmail.com>
2014-11-08 23:36:04 -0800

    media: Use project pathmap
    Change-Id: Ib302016c7d4b9883f3fb5d4f72ca9ed1b3d787d9

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-media)/$(TARGET_BOARD_PLATFORM))
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -15,4 +17,6 @@ endif
 ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092 msm8610 ,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
+endif
+
 endif
