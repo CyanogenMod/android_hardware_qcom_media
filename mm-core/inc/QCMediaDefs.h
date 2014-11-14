ad9d0543f63bf8b029b574616e03a825f988be1e
Haynes Mathew George <hgeorge@codeaurora.org>
2014-11-13 16:36:29 -0800

    audio E_AC3_JOC definition
    Add extern defintion for JOC format
    
    CRs-Fixed: 761339
    Change-Id: I7e007f8bbce18479986def3526986b5f1b6e9c05

--- mm-core/inc/QCMediaDefs.h
+++ mm-core/inc/QCMediaDefs.h
@@ -63,6 +63,7 @@ extern const char *MEDIA_MIMETYPE_CONTAINER_QCMATROSKA;
 extern const char *MEDIA_MIMETYPE_CONTAINER_QCOGG;
 extern const char *MEDIA_MIMETYPE_CONTAINER_QTIFLAC;
 extern const char *MEDIA_MIMETYPE_VIDEO_VPX; //backward compatibility
+extern const char *MEDIA_MIMETYPE_AUDIO_EAC3_JOC;
 }  // namespace android
 
 #endif  //QC_MEDIA_DEFS_H_
