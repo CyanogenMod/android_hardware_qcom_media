48e234c8eda9ef337f298cd9430fddea875a0495
Ethan Chen <intervigil@gmail.com>
2014-11-18 14:12:25 -0800

    media: Use direct project pathmap
    Change-Id: Ib016ef11da0abab60fa43830586d3787c3a6d48e

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media)/$(TARGET_BOARD_PLATFORM))
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
--- dashplayer/Android.mk
+++ dashplayer/Android.mk
@@ -41,8 +41,8 @@ LOCAL_C_INCLUDES := \
 	$(TOP)/frameworks/av/media/libstagefright/include             \
 	$(TOP)/frameworks/av/media/libstagefright/mpeg2ts             \
 	$(TOP)/frameworks/av/media/libstagefright/rtsp                \
-	$(TOP)/$(call project-path-for,qcom-media)/$(TARGET_BOARD_PLATFORM)/mm-core/inc                        \
-	$(TOP)/$(call project-path-for,qcom-display)/$(TARGET_BOARD_PLATFORM)/libgralloc                       \
+	$(TOP)/$(call project-path-for,qcom-media)/mm-core/inc                        \
+	$(TOP)/$(call project-path-for,qcom-display)/libgralloc                       \
 	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
