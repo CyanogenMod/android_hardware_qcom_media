0e80748519c751ec8acc83d250baee908256b0bd
Ethan Chen <intervigil@gmail.com>
2014-11-18 14:05:48 -0800

    media: Use direct project pathmap
    Change-Id: I703cf1882fb1e6cffedc07ad4025d48e630f6236

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media)/default)
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
