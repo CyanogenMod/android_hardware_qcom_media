e2780df91441aee9f82d95a070f3420dcbd35d0a
Shantur Rathore <rathore4u@gmail.com>
2014-11-20 16:53:08 +0000

    Build for MSM 8660
    Change-Id: Ib820a372a55e6617132a6d0350d9b1f3b8e61296

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
 
 include $(call all-subdir-makefiles)
 
