5efba6e02bda3b9109918b9ef674e2041c37732d
Chih-Hung Hsieh <chh@google.com>
2014-12-02 11:32:41 -0800

    Use default system header files.
    Do not specify bionic include directories.
    When compiled with clang/llvm in C++11 mode, libcxx type_traits requires
    standard definition of nullptr_t in cstddef. The bionic version of cstddef
    does not have nullptr_t. This change will allow the compilation to
    include correct cstddef from libcxx.
    
    Change-Id: I12d8cdd608d8f2a4c215b5134c054a8a4001d038

--- mm-video/vidc/venc/Android.mk
+++ mm-video/vidc/venc/Android.mk
@@ -42,7 +42,7 @@ libmm-venc-def += -D_ANDROID_ICS_
 
 include $(CLEAR_VARS)
 
-libmm-venc-inc      += $(LOCAL_PATH)/inc
+libmm-venc-inc      := $(LOCAL_PATH)/inc
 libmm-venc-inc      += $(OMX_VIDEO_PATH)/vidc/common/inc
 libmm-venc-inc      += $(call project-path-for,qcom-media)/mm-core/inc
 libmm-venc-inc      += $(call project-path-for,qcom-media)/libstagefrighthw
