d8b294d780b4e78354a92f946a2d20c8f094f6d2
Deva Ramasubramanian <dramasub@codeaurora.org>
2014-12-04 17:13:01 -0800

    mm-video: vidc: Adjust timestamp if actual ts < previous ts
    The timestamp adjustment logic only bothered adjusting if
    abs(delta) > 2ms.  Hence, if the successive timestamp was less than the
    previous timestamp by more than 2ms, the adjustment never occured.
    
    This commit forces the adjustment to occur if the timestamp appears to
    be going backwards.
    
    Change-Id: I9552a34c48be9813d1300a91126f10691d3d8bf7

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -8522,9 +8522,8 @@ void omx_vdec::adjust_timestamp(OMX_S64 &act_timestamp)
         rst_prev_ts = false;
     } else if (VALID_TS(prev_ts)) {
         bool codec_cond = (drv_ctx.timestamp_adjust)?
-            (!VALID_TS(act_timestamp) || (((act_timestamp > prev_ts)?
-                               (act_timestamp - prev_ts):(prev_ts - act_timestamp)) <= 2000)):
-            (!VALID_TS(act_timestamp) || act_timestamp == prev_ts);
+            (!VALID_TS(act_timestamp) || act_timestamp < prev_ts || llabs(act_timestamp - prev_ts) <= 2000) :
+            (!VALID_TS(act_timestamp) || act_timestamp <= prev_ts);
         if (frm_int > 0 && codec_cond) {
             DEBUG_PRINT_LOW("adjust_timestamp: original ts[%lld]", act_timestamp);
             act_timestamp = prev_ts + frm_int;
