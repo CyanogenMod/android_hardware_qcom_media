0efd2433ec6f7989e3109d88bb8d6e6fb47ef314
Marcos Marado <mmarado@cyngn.com>
2014-12-16 19:31:32 +0000

    CM12vification
    Change-Id: I648f47fa681c1720aac62e83844d2c901c31b58f

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
 $(warning target list is : $(MSM_VIDC_TARGET_LIST))
 
@@ -7,7 +8,7 @@ include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
+ifneq ($(filter msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
@@ -15,4 +16,5 @@ ifeq ($(TARGET_BOARD_PLATFORM),apq8084)
 include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
 endif
 
+endif
 endif
