7f4e8156e46a18688b4f7c8f55ec4000e4875939
Marcos Marado <mmarado@cyngn.com>
2014-12-16 19:31:32 +0000

    CM12vification
    Change-Id: I648f47fa681c1720aac62e83844d2c901c31b58f
    
    Build mm-video-v4l2 on msm8909's
    
    Issue-Id: FEIJ-6
    
    Change-Id: I3eed6c173e1fe2773ad1edebeb4a8035846a470d

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
-$(warning target list is : $(MSM_VIDC_TARGET_LIST))
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
@@ -8,7 +8,7 @@ include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
 ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
+ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
@@ -16,5 +16,6 @@ ifeq ($(TARGET_BOARD_PLATFORM),apq8084)
 include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
 endif
 
+endif
 endif
 endif
--- mm-video-v4l2/vidc/venc/Android.mk
+++ mm-video-v4l2/vidc/venc/Android.mk
@@ -22,10 +22,13 @@ libmm-venc-def += -D_ANDROID_ICS_
 libmm-venc-def += -D_MSM8974_
 
 TARGETS_THAT_USE_FLAG_MSM8226 := msm8226 msm8916
+ifneq "$(wildcard $(QCPATH) )" ""
+# relevant libraries are not built for OSS builds as QCPATH is null in OSS builds.
 ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
 TARGETS_THAT_NEED_SW_VENC_MPEG4 := msm8909
 endif
 TARGETS_THAT_NEED_SW_VENC_HEVC := msm8992
+endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm8610)
 libmm-venc-def += -DMAX_RES_720P
@@ -58,13 +61,13 @@ endif
 # Common Includes
 libmm-venc-inc      := $(LOCAL_PATH)/inc
 libmm-venc-inc      += $(OMX_VIDEO_PATH)/vidc/common/inc
-libmm-venc-inc      += hardware/qcom/media/mm-core/inc
-libmm-venc-inc      += hardware/qcom/media/libstagefrighthw
+libmm-venc-inc      += $(call project-path-for,qcom-media)/mm-core/inc
+libmm-venc-inc      += $(call project-path-for,qcom-media)/libstagefrighthw
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/adreno
 libmm-venc-inc      += frameworks/native/include/media/hardware
 libmm-venc-inc      += frameworks/native/include/media/openmax
-libmm-venc-inc      += hardware/qcom/media/libc2dcolorconvert
+libmm-venc-inc      += $(call project-path-for,qcom-media)/libc2dcolorconvert
 libmm-venc-inc      += frameworks/av/include/media/stagefright
 libmm-venc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
