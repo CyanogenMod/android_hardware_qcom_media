c8b6d5ceaf831f7b0ba551687eaa099b05cdb156
Marcos Marado <mmarado@cyngn.com>
2014-12-16 19:31:32 +0000

    CM12vification
    Change-Id: I648f47fa681c1720aac62e83844d2c901c31b58f

--- mm-video-v4l2/vidc/common/Android.mk
+++ mm-video-v4l2/vidc/common/Android.mk
@@ -25,9 +25,9 @@ libmm-vidc-add-dep  := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 # ---------------------------------------------------------------------------------
 
 libmm-vidc-inc      := $(LOCAL_PATH)/inc
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/mm-core/inc
+libmm-vidc-inc      += $(TOP)/$(call project-path-for,qcom-media)/mm-core/inc
 libmm-vidc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/libc2dcolorconvert
+libmm-vidc-inc      += $(TOP)/$(call project-path-for,qcom-media)/libc2dcolorconvert
 libmm-vidc-inc      += $(TOP)/frameworks/av/include/media/stagefright
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
