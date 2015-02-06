fff27a1f3123185e9033fa97b5a7f72ee21b532b
Abdulla Anam <abdullahanam@codeaurora.org>
2015-02-06 16:42:39 +0530

    mm-video-v4l2: vidc: venc: make buffer index data type consistence
    In encoder component buffer index is calculated using omx header and
    header array address at different places. There is an inconsistency
    in buffer index data type across different functions. Make it
    unsigned int, as array index should always be a positive integer
    number. Do required typecasting and make sanity check changes as per
    requirement.
    
    Change-Id: I9b9a8e92c111ac6907754b3cf2226feb09d62764

--- mm-video-v4l2/vidc/venc/inc/omx_video_base.h
+++ mm-video-v4l2/vidc/venc/inc/omx_video_base.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
+Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -495,7 +495,7 @@ class omx_video: public qc_omx_component
                 OMX_BUFFERHEADERTYPE *buffer);
         OMX_ERRORTYPE push_input_buffer(OMX_HANDLETYPE hComp);
         OMX_ERRORTYPE convert_queue_buffer(OMX_HANDLETYPE hComp,
-                struct pmem &Input_pmem_info,unsigned long &index);
+                struct pmem &Input_pmem_info,unsigned long index);
         OMX_ERRORTYPE queue_meta_buffer(OMX_HANDLETYPE hComp,
                 struct pmem &Input_pmem_info);
         OMX_ERRORTYPE push_empty_eos_buffer(OMX_HANDLETYPE hComp,
