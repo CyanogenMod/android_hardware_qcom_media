8ba01d1fc104e676676307dbe680c692c3653b9d
Ethan Chen <intervigil@gmail.com>
2015-02-23 22:59:57 -0500

    media: Use project pathmap
    Change-Id: I4f2e1f666deded6d53f23c97718b4246b48cf780

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -12,6 +14,8 @@ include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
 ifeq ($(TARGET_BOARD_PLATFORM),apq8084)
 include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
+endif
+
 endif
 
 endif
