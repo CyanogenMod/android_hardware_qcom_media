34c67df39d646c3d51e786147c21e485eb38d9b6
Praveen Chavan <pchavan@codeaurora.org>
2015-04-20 18:52:19 -0700

    mm-video-v4l2: Add extended config to set instance priority
    Enable priority as hinted by the client:
    priority = 0 => real-time
             = 1 => non-realtime
    Also add the OMX extension for priority and operating-rate.
    
    Amended by Ashray Kulkarni <ashrayk@codeaurora.org>
    Default sessions to non-realtime.
    
    bug: 20131548
    Change-Id: I37532404a4a3ab5647b3490c3e3ecba2ed604bbf

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -253,6 +253,7 @@ venc_dev::venc_dev(class omx_venc *venc_class)
     enable_mv_narrow_searchrange = false;
     supported_rc_modes = RC_ALL;
     memset(&ltrinfo, 0, sizeof(ltrinfo));
+    sess_priority.priority = 1;
 
     char property_value[PROPERTY_VALUE_MAX] = {0};
     property_get("vidc.enc.log.in", property_value, "0");
@@ -1050,6 +1051,11 @@ bool venc_dev::venc_open(OMX_U32 codec)
         }
     }
 
+    sess_priority.priority = 1; /* default to non-real-time */
+    if (venc_set_session_priority(sess_priority.priority)) {
+        DEBUG_PRINT_ERROR("Setting session priority failed");
+        return OMX_ErrorUnsupportedSetting;
+    }
     return true;
 }
 
@@ -2079,6 +2085,17 @@ bool venc_dev::venc_set_config(void *configData, OMX_INDEXTYPE index)
                 }
                 break;
             }
+        case OMX_IndexConfigPriority:
+            {
+                OMX_PARAM_U32TYPE *priority = (OMX_PARAM_U32TYPE *)configData;
+                DEBUG_PRINT_LOW("Set_config: priority %u",priority->nU32);
+                if (!venc_set_session_priority(priority->nU32)) {
+                    DEBUG_PRINT_ERROR("Failed to set priority");
+                    return false;
+                }
+                break;
+            }
+
         default:
             DEBUG_PRINT_ERROR("Unsupported config index = %u", index);
             break;
@@ -2305,6 +2322,8 @@ void venc_dev::venc_config_print()
     DEBUG_PRINT_HIGH("ENC_CONFIG: VUI timing info enabled: %d", vui_timing_info.enabled);
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: Peak bitrate: %d", peak_bitrate.peakbitrate);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: Session Priority: %u", sess_priority.priority);
 }
 
 bool venc_dev::venc_reconfig_reqbufs()
@@ -4391,6 +4410,37 @@ bool venc_dev::venc_set_vpx_error_resilience(OMX_BOOL enable)
     }
     vpx_err_resilience.enable = 1;
     DEBUG_PRINT_LOW("Success IOCTL set control for id=%d, value=%d", control.id, control.value);
+    return true;
+}
+
+bool venc_dev::venc_set_session_priority(OMX_U32 priority) {
+    struct v4l2_control control;
+
+    control.id = V4L2_CID_MPEG_VIDC_VIDEO_PRIORITY;
+    switch(priority) {
+        case 0:
+            control.value = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_ENABLE;
+            break;
+        case 1:
+            control.value = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_DISABLE;
+            break;
+        default:
+            priority = 1;
+            control.value = V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_DISABLE;
+            DEBUG_PRINT_ERROR("Unsupported priority level %u", priority);
+            break;
+    }
+
+    if (ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control)) {
+        DEBUG_PRINT_ERROR("Failed to set V4L2_MPEG_VIDC_VIDEO_PRIORITY_REALTIME_%s",
+                priority == 0 ? "ENABLE" : "DISABLE");
+        return false;
+    }
+
+    sess_priority.priority = priority;
+
+    DEBUG_PRINT_LOW("Success IOCTL set control for id=%x, val=%d",
+            control.id, control.value);
     return true;
 }
 
