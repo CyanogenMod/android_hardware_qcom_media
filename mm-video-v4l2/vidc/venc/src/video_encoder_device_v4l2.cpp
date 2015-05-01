b716acdf7f0dff9d7fe1760c08dc0c0fed072df0
Praveen Chavan <pchavan@codeaurora.org>
2015-04-30 20:28:44 -0700

    mm-video-v4l2: vidc: add support for setting operating-rate
    Operating-rate hints the incoming rate of frames in
    real-time which can be different from the content fps.
    This rate will use used to set the correct operating
    point for encoder and decoder.
    
    This config will fail with InSufficientResources if the
    hardware cannot accomodate the requested operating rate.
    
    Amended by Ashray Kulkarni <ashrayk@codeaurora.org>
    
    bug: 20134262
    Change-Id: If3f21067e31a4646ba143b1acd2d075421a6277a

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -259,6 +259,7 @@ venc_dev::venc_dev(class omx_venc *venc_class)
     camera_mode_enabled = false;
     memset(&ltrinfo, 0, sizeof(ltrinfo));
     sess_priority.priority = 1;
+    operating_rate = 0;
 
     char property_value[PROPERTY_VALUE_MAX] = {0};
     property_get("vidc.enc.log.in", property_value, "0");
@@ -2158,7 +2159,16 @@ bool venc_dev::venc_set_config(void *configData, OMX_INDEXTYPE index)
                 }
                 break;
             }
-
+        case OMX_IndexConfigOperatingRate:
+            {
+                OMX_PARAM_U32TYPE *rate = (OMX_PARAM_U32TYPE *)configData;
+                DEBUG_PRINT_LOW("Set_config: operating rate %d", rate->nU32);
+                if (!venc_set_operatingrate(rate->nU32)) {
+                    DEBUG_PRINT_ERROR("Failed to set operating rate");
+                    return false;
+                }
+                break;
+            }
         default:
             DEBUG_PRINT_ERROR("Unsupported config index = %u", index);
             break;
@@ -2404,6 +2414,8 @@ void venc_dev::venc_config_print()
     DEBUG_PRINT_HIGH("ENC_CONFIG: Peak bitrate: %d", peak_bitrate.peakbitrate);
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: Session Priority: %u", sess_priority.priority);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: Operating Rate: %u", operating_rate);
 }
 
 bool venc_dev::venc_reconfig_reqbufs()
@@ -4731,6 +4743,26 @@ bool venc_dev::venc_set_session_priority(OMX_U32 priority) {
 
     DEBUG_PRINT_LOW("Success IOCTL set control for id=%x, val=%d",
             control.id, control.value);
+    return true;
+}
+
+bool venc_dev::venc_set_operatingrate(OMX_U32 rate) {
+    struct v4l2_control control;
+
+    control.id = V4L2_CID_MPEG_VIDC_VIDEO_OPERATING_RATE;
+    control.value = rate;
+
+    DEBUG_PRINT_LOW("venc_set_operating_rate: %d fps", rate >> 16);
+    DEBUG_PRINT_LOW("Calling IOCTL set control for id=%d, val=%d", control.id, control.value);
+
+    if(ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control)) {
+        hw_overload = errno == EBUSY;
+        DEBUG_PRINT_ERROR("Failed to set operating rate %d fps (%s)",
+                rate >> 16, hw_overload ? "HW overload" : strerror(errno));
+        return false;
+    }
+    operating_rate = rate;
+    DEBUG_PRINT_LOW("Operating Rate Set = %d fps",  rate >> 16);
     return true;
 }
 
