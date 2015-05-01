dce0acfcb3cdf6eceef84f22def392cc6d4557ab
Praveen Chavan <pchavan@codeaurora.org>
2015-04-30 20:28:44 -0700

    mm-video-v4l2: vidc: add support for setting operating-rate
    Operating-rate hints the incoming rate of frames in
    real-time which can be different from the content fps.
    This rate will use used to set the correct operating point
    for encoder and decoder.
    
    This config will fail with InSufficientResources if the
    hardware cannot accomodate the requested operating rate.
    
    Amended by Ashray Kulkarni <ashrayk@codeaurora.org>
    
    bug: 20134262
    Change-Id: If3f21067e31a4646ba143b1acd2d075421a6277a

--- mm-video-v4l2/vidc/venc/inc/video_encoder_device_v4l2.h
+++ mm-video-v4l2/vidc/venc/inc/video_encoder_device_v4l2.h
@@ -357,6 +357,7 @@ class venc_dev
         struct msm_venc_ltrinfo             ltrinfo;
         struct msm_venc_vpx_error_resilience vpx_err_resilience;
         struct msm_venc_priority            sess_priority;
+        OMX_U32                             operating_rate;
 
         bool venc_set_profile_level(OMX_U32 eProfile,OMX_U32 eLevel);
         bool venc_set_intra_period(OMX_U32 nPFrames, OMX_U32 nBFrames);
@@ -398,6 +399,7 @@ class venc_dev
         bool venc_calibrate_gop();
         bool venc_validate_hybridhp_params(OMX_U32 layers, OMX_U32 bFrames, OMX_U32 count, int mode);
         bool venc_set_session_priority(OMX_U32 priority);
+        bool venc_set_operatingrate(OMX_U32 rate);
 
 #ifdef MAX_RES_1080P
         OMX_U32 pmem_free();
