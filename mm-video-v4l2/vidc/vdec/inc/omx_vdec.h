8c9751508d6651ef9634f357b85b4c17848c7fa4
Deva Ramasubramanian <dramasub@codeaurora.org>
2015-05-07 11:56:27 -0700

    mm-video-v4l2: vidc: vdec: Add support for VQZIP SEI
    Enabling OMX_QTIIndexParamVQZIPSEIType allows the client to
    receive the VQZIP related extradatas of the decoded bitstream,
    if it was encoded by the Venus encoder.
    
    Change-Id: If420291663b80c7e1bd96847c20bb07ef9424c8e

--- mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
+++ mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -192,7 +192,7 @@ class VideoHeap : public MemoryHeapBase
 #define OMX_FRAMEPACK_EXTRADATA 0x00400000
 #define OMX_QP_EXTRADATA        0x00800000
 #define OMX_BITSINFO_EXTRADATA  0x01000000
-#define DRIVER_EXTRADATA_MASK   0x0000FFFF
+#define OMX_VQZIPSEI_EXTRADATA  0x02000000
 
 #define OMX_INTERLACE_EXTRADATA_SIZE ((sizeof(OMX_OTHER_EXTRADATATYPE) +\
             sizeof(OMX_STREAMINTERLACEFORMAT) + 3)&(~3))
@@ -208,6 +208,8 @@ class VideoHeap : public MemoryHeapBase
             sizeof(OMX_QCOM_EXTRADATA_QP) + 3)&(~3))
 #define OMX_BITSINFO_EXTRADATA_SIZE ((sizeof(OMX_OTHER_EXTRADATATYPE) +\
             sizeof(OMX_QCOM_EXTRADATA_BITS_INFO) + 3)&(~3))
+#define OMX_VQZIPSEI_EXTRADATA_SIZE ((sizeof(OMX_OTHER_EXTRADATATYPE) +\
+            sizeof(OMX_QCOM_EXTRADATA_VQZIPSEI) + 3)&(~3))
 #define OMX_USERDATA_EXTRADATA_SIZE ((sizeof(OMX_OTHER_EXTRADATATYPE) +\
             + 3)&(~3))
 
@@ -690,6 +692,8 @@ class omx_vdec: public qc_omx_component
                 struct msm_vidc_frame_qp_payload *qp_payload);
         void append_bitsinfo_extradata(OMX_OTHER_EXTRADATATYPE *extra,
                 struct msm_vidc_frame_bits_info_payload *bits_payload);
+        void append_vqzip_extradata(OMX_OTHER_EXTRADATATYPE *extra,
+                struct msm_vidc_vqzip_sei_payload *vqzip_payload);
         void insert_demux_addr_offset(OMX_U32 address_offset);
         void extract_demux_addr_offsets(OMX_BUFFERHEADERTYPE *buf_hdr);
         OMX_ERRORTYPE handle_demux_data(OMX_BUFFERHEADERTYPE *buf_hdr);
