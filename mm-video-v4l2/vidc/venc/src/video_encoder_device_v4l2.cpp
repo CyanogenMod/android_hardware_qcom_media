3c20d8e49e67e8eacdd08a0f18c02f996fcff9d1
Praveen Chavan <pchavan@codeaurora.org>
2015-05-08 11:28:24 -0700

    mm-video-v4l2: venc: report HW overload as insufficientResources
    Check for HW overload when setting format and propagate it
    as OMX_ErrorInsuffcientResources
    
    Bug: 20132289
    Change-Id: I94eea3913ceb18f82cf7436f1f41bb3f1ec75324

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -1319,6 +1319,7 @@ bool venc_dev::venc_set_param(void *paramData, OMX_INDEXTYPE index)
 
                         if (ioctl(m_nDriver_fd, VIDIOC_S_FMT, &fmt)) {
                             DEBUG_PRINT_ERROR("VIDIOC_S_FMT OUTPUT_MPLANE Failed");
+                            hw_overload = errno == EBUSY;
                             return false;
                         }
 
@@ -1351,6 +1352,7 @@ bool venc_dev::venc_set_param(void *paramData, OMX_INDEXTYPE index)
 
                     if (ioctl(m_nDriver_fd, VIDIOC_S_FMT, &fmt)) {
                         DEBUG_PRINT_ERROR("VIDIOC_S_FMT CAPTURE_MPLANE Failed");
+                        hw_overload = errno == EBUSY;
                         return false;
                     }
 
