790d37a7dce1225867cd93281ed3340a68d194ce
Narsinga Rao Chella <nrchella@codeaurora.org>
2015-05-29 14:09:59 -0700

    mm-core: add QTI OMX extensions for FLAC
    Add QTI OMX extensions to support QTI FLAC SW decoder.
    
    Change-Id: I558886af65fcbd371ac539bd3e5e6935114164df

--- mm-core/inc/QOMX_AudioExtensions.h
+++ mm-core/inc/QOMX_AudioExtensions.h
@@ -77,6 +77,7 @@ extern "C"
 #define OMX_QCOM_INDEX_CONFIG_AAC_SEL_MIX_COEF "OMX.Qualcomm.index.audio.aac_sel_mix_coef"
 #define OMX_QCOM_INDEX_PARAM_ALAC            "OMX.Qualcomm.index.audio.alac"
 #define OMX_QCOM_INDEX_PARAM_APE             "OMX.Qualcomm.index.audio.ape"
+#define OMX_QCOM_INDEX_PARAM_FLAC_DEC        "OMX.Qualcomm.index.audio.flacdec"
 
 typedef enum QOMX_AUDIO_AMRBANDMODETYPE {
     QOMX_AUDIO_AMRBandModeWB9              = 0x7F000001,/**< AMRWB Mode 9 = SID*/
@@ -540,6 +541,19 @@ typedef struct QOMX_AUDIO_PARAM_APETYPE {
     OMX_U32 nSampleRate; /* Samples per second in Hertz */
     OMX_U32 nSeekTablePresent; /* Flag to indicate if seek table is present or not */
 } QOMX_AUDIO_PARAM_APETYPE;
+
+typedef struct QOMX_AUDIO_PARAM_FLAC_DEC_TYPE {
+    OMX_U32 nSize;
+    OMX_VERSIONTYPE nVersion;
+    OMX_U32 nPortIndex;
+    OMX_U32 nChannels;
+    OMX_U32 nSampleRate;
+    OMX_U32 nBitsPerSample;
+    OMX_U32 nMinBlkSize;
+    OMX_U32 nMaxBlkSize;
+    OMX_U32 nMinFrmSize;
+    OMX_U32 nMaxFrmSize;
+} QOMX_AUDIO_PARAM_FLAC_DEC_TYPE;
 
 #if defined( __cplusplus )
 }
