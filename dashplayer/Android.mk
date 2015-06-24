dba63daa7818f98ac6a6be78f8c7d20d332fa70f
Rashed Abdel-Tawab <rashed@linux.com>
2015-06-24 17:45:07 -0400

    media: msm8994: Import QCMediaPlayer and dashplayer
    Change-Id: I22748486e810b89989829fc8a578fa542c4991ab

---
+++ dashplayer/Android.mk
@@ -0,0 +1,58 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# ---------------------------------------------------------------------------------
+#            Common definitons
+# ---------------------------------------------------------------------------------
+LOCAL_SRC_FILES:=                       \
+        DashPlayer.cpp                  \
+        DashPlayerDriver.cpp            \
+        DashPlayerRenderer.cpp          \
+        DashPlayerStats.cpp             \
+        DashPlayerDecoder.cpp           \
+        DashPacketSource.cpp            \
+        DashFactory.cpp
+
+LOCAL_SHARED_LIBRARIES :=       \
+    libbinder                   \
+    libcamera_client            \
+    libcutils                   \
+    libdl                       \
+    libgui                      \
+    libmedia                    \
+    libstagefright              \
+    libstagefright_foundation   \
+    libstagefright_omx          \
+    libutils                    \
+    libui                       \
+
+LOCAL_C_INCLUDES := \
+        $(TOP)/frameworks/av/media/libstagefright/timedtext           \
+	$(TOP)/frameworks/native/include/media/hardware               \
+	$(TOP)/frameworks/native/include/media/openmax                \
+	$(TOP)/frameworks/av/media/libstagefright/httplive            \
+	$(TOP)/frameworks/av/media/libmediaplayerservice/nuplayer     \
+	$(TOP)/frameworks/av/media/libmediaplayerservice              \
+	$(TOP)/frameworks/av/media/libstagefright/include             \
+	$(TOP)/frameworks/av/media/libstagefright/mpeg2ts             \
+	$(TOP)/frameworks/av/media/libstagefright/rtsp                \
+	$(TOP)/hardware/qcom/media/mm-core/inc                        \
+	$(TOP)/hardware/qcom/display/libgralloc                       \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+ifeq ($(PLATFORM_SDK_VERSION), 18)
+  LOCAL_CFLAGS += -DANDROID_JB_MR2
+endif
+
+LOCAL_CFLAGS += -DBFAMILY_TARGET
+
+LOCAL_MODULE:= libdashplayer
+
+LOCAL_MODULE_TAGS := eng
+
+ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
+ include $(BUILD_SHARED_LIBRARY)
+endif
+
