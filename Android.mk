d085d7c0e6f48edb9be361de84a9717255435838
Michael Bestas <mikeioannina@gmail.com>
2015-07-07 03:39:46 +0300

    mm-video-v4l2: Remove useless build warning
    * Get rid of "hardware/qcom/media-caf/msm8916/Android.mk:3: target list is : "
    
    Change-Id: I904125d297218113c6f777c1a2e7883eea5141d4

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
-$(warning target list is : $(MSM_VIDC_TARGET_LIST))
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
