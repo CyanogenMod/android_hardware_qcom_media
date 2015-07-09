29f5088732151cda71aace63cddeac185c0f35cc
Pradosh Das <prados@codeaurora.org>
2015-07-09 10:38:19 +0530

    mm-video-v4l2: Fix compilation issues due to C++11
    Add necessary include files and fix compilation errors.
    
    Change-Id: I05d3f9d514ddb212f0a8cd67a92c74299b14bfdb

--- libc2dcolorconvert/C2DColorConverter.h
+++ libc2dcolorconvert/C2DColorConverter.h
@@ -33,6 +33,8 @@
 #include <c2d2.h>
 #include <ColorConverter.h>
 #include <sys/types.h>
+#include <string.h>
+#include <errno.h>
 
 typedef C2D_STATUS (*LINK_c2dCreateSurface)( uint32 *surface_id,
         uint32 surface_bits,
