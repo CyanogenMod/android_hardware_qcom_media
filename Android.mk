ed83ecaed0c7dcd40eeb9e0faa13ee782edfa944
Steve Paik <spaik@google.com>
2015-07-15 16:51:55 -0700

    Workaround for adp8064
    This is a squash of I9ddf81a0008e4798b7b499bf08859bae37eef947
    and Id8a1d9b75d5bbbcf5dccacf28f7f40da746990f4
    
    Change-Id: I0477794381f1a90fd1a87ca63cb2fda7959b7aec

--- Android.mk
+++ Android.mk
@@ -1,17 +1,20 @@
-QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
-include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-endif
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  QCOM_MEDIA_ROOT := $(call my-dir)
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8660 msm7627a msm7630_surf msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+    include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+    include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+  endif
 
-ifneq ($(filter msm8960 msm8660,$(TARGET_BOARD_PLATFORM)),)
-include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
-endif
+  ifneq ($(filter msm8960 msm8660,$(TARGET_BOARD_PLATFORM)),)
+    include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
+  endif
 
-ifneq ($(filter msm8610 msm8226 msm8974 msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
-include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
-endif
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+    include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+  endif
 
-ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
-include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+  ifneq ($(filter msm8610 msm8226 msm8974 msm8960 msm8084 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+    include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
+  endif
 endif
