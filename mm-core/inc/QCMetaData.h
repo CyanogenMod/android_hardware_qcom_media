5fe633cb54d1b8e99de08efc7e02017746a6197d
Manish Dewangan <manish@codeaurora.org>
2015-08-12 18:42:12 +0530

    audio: Add support for new key kKeyByteMode
    -Add support for new key kKeyByteMode
    -For the audio formats which supports BYTE MODE, Decoder input buffer
    will be configured by parser using this key.
    
    Change-Id: Ie621bf17dfe6444ae73fb6174438160286ee29bd

--- mm-core/inc/QCMetaData.h
+++ mm-core/inc/QCMetaData.h
@@ -73,6 +73,7 @@ enum {
     kKeyMinFrmSize        = 'mifs', //int32_t
     kKeyMaxFrmSize        = 'mafs', //int32_t
     kKeyMd5Sum            = 'md5s', //cstring
+    kKeyIsByteMode        = 'bytm', //int32_t
 };
 
 enum {
