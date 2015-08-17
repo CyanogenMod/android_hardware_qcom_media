e2df3bd94ada7474143a128954555538261829ad
Manikanta Sivapala <msivap@codeaurora.org>
2015-08-17 16:39:01 +0530

    dashplayer: Bringing jni related code for DASH
    Bringing jni related code for DASH
    
    Change-Id: I109e9d13610a8fb51264dce0be51361af5a778ab

---
+++ dashplayer/jni/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    android_media_ExtMediaPlayer.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+    libandroid_runtime \
+    libnativehelper \
+    libutils \
+    libbinder \
+    liblog \
+    libstagefright \
+    libstagefright_foundation \
+    libdl \
+
+LOCAL_C_INCLUDES += \
+    $(TOP)/frameworks/base/core/jni \
+    $(TOP)/frameworks/av/media/libstagefright \
+
+LOCAL_MODULE:= libextmedia_jni
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
