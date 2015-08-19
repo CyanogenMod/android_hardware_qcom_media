7f64afefcc8b9f597582fc1aa5b799c150c2f443
Manikanta Sivapala <msivap@codeaurora.org>
2015-08-19 16:06:25 +0530

    dashplayer: Create native qcmediaplayer
    create native qcmediaplayer by extending mediaplayer
    functionality
    
    Change-Id: Icd403fb3c58a42741c008ebe8c133d6bfe8f9bae

--- Android.mk
+++ Android.mk
@@ -14,6 +14,7 @@ endif
 
 ifneq ($(filter msm8974 msm8960 msm8226 apq8084 mpq8092 msm8610 ,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
+include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/native/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/dashplayer/jni/Android.mk
 endif
