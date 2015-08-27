a38abf10e2c61f5f6b1ddd6fa5c4a7ab5c9fd37c
Domi Papoi <dpapoi@codeaurora.org>
2015-08-27 16:50:31 -0400

    mm-video: vdec: Fix for CTS test case VP8Flex1080p
    Check for pointer NULL and return value.
    
    Change-Id: Ic6b8500e2160b402a2d5087177bfb3125b2a74e8

--- mm-video/vidc/vdec/inc/omx_vdec.h
+++ mm-video/vidc/vdec/inc/omx_vdec.h
@@ -876,7 +876,7 @@ private:
              OMX_U32 bytes, OMX_U8 *buffer);
         OMX_ERRORTYPE free_output_buffer(OMX_BUFFERHEADERTYPE *bufferHdr);
         void enable_native_buffers(bool enable) {m_native_buffers_enabled = enable;}
-        #define MAX_COUNT 32
+#define MAX_COUNT MAX_NUM_INPUT_OUTPUT_BUFFERS
         OMX_BUFFERHEADERTYPE  m_out_mem_ptr_client[MAX_COUNT];
     private:
         omx_vdec *omx;
@@ -893,7 +893,9 @@ private:
         OMX_QCOM_PLATFORM_PRIVATE_LIST      m_platform_list_client[MAX_COUNT];
         OMX_QCOM_PLATFORM_PRIVATE_ENTRY     m_platform_entry_client[MAX_COUNT];
         OMX_QCOM_PLATFORM_PRIVATE_PMEM_INFO m_pmem_info_client[MAX_COUNT];
+#ifdef USE_ION
         struct vdec_ion op_buf_ion_info[MAX_COUNT];
+#endif
         unsigned char *pmem_baseaddress[MAX_COUNT];
         int pmem_fd[MAX_COUNT];
         struct vidc_heap
