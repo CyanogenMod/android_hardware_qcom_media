610c3563881b6971dce026b9f8463d08fe1e0494
Ethan Chen <intervigil@gmail.com>
2015-12-24 20:22:06 -0800

    media: Use project pathmap
    Change-Id: Id1851644fef3756caa0b9e8774fb68dd90ca4392

--- mm-video-v4l2/vidc/common/Android.mk
+++ mm-video-v4l2/vidc/common/Android.mk
@@ -21,9 +21,9 @@ libmm-vidc-def += -D_ANDROID_ICS_
 # ---------------------------------------------------------------------------------
 
 libmm-vidc-inc      := $(LOCAL_PATH)/inc
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/mm-core/inc
+libmm-vidc-inc      += $(TOP)/$(call project-path-for,qcom-media)/mm-core/inc
 libmm-vidc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/libc2dcolorconvert
+libmm-vidc-inc      += $(TOP)/$(call project-path-for,qcom-media)/libc2dcolorconvert
 libmm-vidc-inc      += $(TOP)/frameworks/av/include/media/stagefright
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
