73699c1931234002157fb99169694a43ce61924a
Deepak Kushwah <dkushwah@codeaurora.org>
2016-01-07 18:34:24 +0530

    mm-video-v4l2: Pointer null check before deferencing it
    Pointer needs to be checked for sanity before dereferencing
    it.
    
    Change-Id: I4734b581cb6b506ebad9c99b8c9f407bccb4c3d1

--- mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_video_base.cpp
@@ -3334,6 +3334,10 @@ OMX_ERRORTYPE  omx_video::allocate_output_buffer(
                 //secure handle fd struct native_handle_t*
                 m_pOutput_pmem[i].buffer = malloc(sizeof(OMX_U32) + sizeof(native_handle_t*));
                 native_handle_t *handle = native_handle_create(1, 0);
+                if (!handle) {
+                    DEBUG_PRINT_ERROR("ERROR: native handle creation failed");
+                    return OMX_ErrorInsufficientResources;
+                }
                 handle->data[0] = m_pOutput_pmem[i].fd;
                 char *data = (char*) m_pOutput_pmem[i].buffer;
                 OMX_U32 type = 1;
