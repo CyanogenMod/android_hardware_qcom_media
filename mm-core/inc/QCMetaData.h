b996b13eea2c38ad220c5172fa1139b7ef4f03bf
Domi Papoi <dpapoi@codeaurora.org>
2016-01-11 16:31:57 -0500

    QCMetadata: Add key for setBuffers API support
    - QTI Parser should set this key when setBuffers API is
      supported
    
    Change-Id: Ib90b83fbdfd6bf4731e4d1be0605c609cfa43803

--- mm-core/inc/QCMetaData.h
+++ mm-core/inc/QCMetaData.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -78,6 +78,7 @@ enum {
 
     kKeyBatchSize            = 'btch', //int32_t
     kKeyIsByteMode           = 'bytm', //int32_t
+    kKeyUseSetBuffers        = 'setb', //bool (int32_t)
 };
 
 #if 0
@@ -98,6 +99,10 @@ enum {
 //Value should reflect as last entry in the enum
 enum {
     CAN_SEEK_TO_ZERO   = 16, // the "previous button"
+};
+
+enum {
+    USE_SET_BUFFERS = 1,
 };
 }  // namespace android
 
