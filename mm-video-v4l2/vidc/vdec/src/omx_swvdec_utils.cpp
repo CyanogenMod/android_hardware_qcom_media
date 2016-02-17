c911dc31a3a2b6108ca41ab4b2c907cd64178f99
Manikanta Sivapala <msivap@codeaurora.org>
2016-02-17 16:05:20 +0530

    mm-video: swvdec: Use C++ standard library queue classes.
    - Use C++ standard library 'queue' class in 'omx_swvdec_queue' class.
    - Use C++ standard library 'priority queue' class for timestamp
      queue.
    - Change return type of async_post_event() & omx_swvdec_queue::push()
      from 'bool' to 'void'.
    
    Change-Id: I917918d634aaf10b93795ff55e420868a90e50e8

--- mm-video-v4l2/vidc/vdec/src/omx_swvdec_utils.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_swvdec_utils.cpp
@@ -86,14 +86,6 @@ void omx_swvdec_log_init()
  */
 omx_swvdec_queue::omx_swvdec_queue()
 {
-    memset(m_queue, 0, sizeof(m_queue));
-
-    m_count_total  = OMX_SWVDEC_QUEUE_ELEMENTS;
-    m_count_filled = 0;
-
-    m_index_write  = 0;
-    m_index_read   = 0;
-
     pthread_mutex_init(&m_mutex, NULL);
 }
 
@@ -109,32 +101,14 @@ omx_swvdec_queue::~omx_swvdec_queue()
  * @brief Push event to queue.
  *
  * @param[in] p_event_info: Pointer to event information structure.
- *
- * @retval  true if push successful
- * @retval false if push unsuccessful
  */
-bool omx_swvdec_queue::push(OMX_SWVDEC_EVENT_INFO *p_event_info)
+void omx_swvdec_queue::push(OMX_SWVDEC_EVENT_INFO *p_event_info)
 {
-    bool retval = true;
-
     pthread_mutex_lock(&m_mutex);
 
-    if (m_count_filled < m_count_total)
-    {
-        m_queue[m_index_write] = *p_event_info;
-
-        m_index_write = (m_index_write + 1) % m_count_total;
-
-        m_count_filled++;
-    }
-    else
-    {
-        retval = false;
-    }
+    m_queue.push(*p_event_info);
 
     pthread_mutex_unlock(&m_mutex);
-
-    return retval;
 }
 
 /**
@@ -151,147 +125,15 @@ bool omx_swvdec_queue::pop(OMX_SWVDEC_EVENT_INFO *p_event_info)
 
     pthread_mutex_lock(&m_mutex);
 
-    if (m_count_filled > 0)
-    {
-        *p_event_info = m_queue[m_index_read];
-
-        memset(&m_queue[m_index_read], 0, sizeof(OMX_SWVDEC_EVENT_INFO));
-
-        m_index_read = (m_index_read + 1) % m_count_total;
-
-        m_count_filled--;
-    }
-    else
+    if (m_queue.empty())
     {
         retval = false;
     }
-
-    pthread_mutex_unlock(&m_mutex);
-
-    return retval;
-}
-
-/**
- * @brief OMX SwVdec timestamp list constructor.
- */
-omx_swvdec_ts_list::omx_swvdec_ts_list()
-{
-    reset();
-
-    pthread_mutex_init(&m_mutex, NULL);
-}
-
-/**
- * @brief OMX SwVdec timestamp list destructor.
- */
-omx_swvdec_ts_list::~omx_swvdec_ts_list()
-{
-    pthread_mutex_destroy(&m_mutex);
-}
-
-/**
- * @brief Reset timestamp list.
- */
-void omx_swvdec_ts_list::reset()
-{
-    memset(m_list, 0, sizeof(m_list));
-
-    m_count_filled = 0;
-}
-
-/**
- * @brief Push timestamp to list, keeping lowest-valued timestamp at the end.
- *
- * @param[in] timestamp: Timestamp.
- *
- * @retval  true if push successful
- * @retval false if push unsuccessful
- */
-bool omx_swvdec_ts_list::push(long long timestamp)
-{
-    bool retval;
-
-    pthread_mutex_lock(&m_mutex);
-
-    if (m_count_filled < OMX_SWVDEC_TS_LIST_ELEMENTS)
-    {
-        int index_curr, index_prev;
-
-        long long timestamp_tmp;
-
-        // insert timestamp into list
-
-        m_list[m_count_filled].filled    = true;
-        m_list[m_count_filled].timestamp = timestamp;
-
-        m_count_filled++;
-
-        // iterate backwards
-
-        index_curr = m_count_filled - 1;
-        index_prev = m_count_filled - 2;
-
-        while ((index_curr > 0) &&
-               (m_list[index_curr].timestamp > m_list[index_prev].timestamp))
-        {
-            // swap timestamps
-
-            timestamp_tmp                = m_list[index_prev].timestamp;
-            m_list[index_prev].timestamp = m_list[index_curr].timestamp;
-            m_list[index_curr].timestamp = timestamp_tmp;
-
-            index_curr--;
-            index_prev--;
-        }
-
-        OMX_SWVDEC_LOG_LOW("timestamp %lld pushed", timestamp);
-
-        retval = true;
-    }
     else
     {
-        OMX_SWVDEC_LOG_ERROR("timestamp %lld push failed; list full",
-                             timestamp);
+        *p_event_info = m_queue.front();
 
-        retval = false;
-    }
-
-    pthread_mutex_unlock(&m_mutex);
-
-    return retval;
-}
-
-/**
- * @brief Pop timestamp from list.
- *
- * @param[in,out] p_timestamp: Pointer to timestamp variable.
- *
- * @retval  true if pop successful
- * @retval false if pop unsuccessful
- */
-bool omx_swvdec_ts_list::pop(long long *p_timestamp)
-{
-    bool retval;
-
-    pthread_mutex_lock(&m_mutex);
-
-    if (m_count_filled)
-    {
-        *p_timestamp = m_list[m_count_filled - 1].timestamp;
-
-        m_list[m_count_filled - 1].filled = false;
-
-        m_count_filled--;
-
-        OMX_SWVDEC_LOG_LOW("timestamp %lld popped", *p_timestamp);
-
-        retval = true;
-    }
-    else
-    {
-        OMX_SWVDEC_LOG_ERROR("timestamp pop failed; list empty");
-
-        retval = false;
+        m_queue.pop();
     }
 
     pthread_mutex_unlock(&m_mutex);
