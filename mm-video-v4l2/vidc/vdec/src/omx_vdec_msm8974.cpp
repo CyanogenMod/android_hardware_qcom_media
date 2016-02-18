92b55f1e9bd5f9a4ab37e2a7d4541039f0b63f0d
Arun Menon <avmenon@codeaurora.org>
2016-02-17 18:55:28 -0800

    mm-video-v4l2: vdec: Enhance video resolution check
    Enhance the video resolution check to ensure that unsupported
    resolutions do not get supported.
    
    Change-Id: I4ff81a51178bc40fccf78cb003c5c369a1b3573d

--- mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
+++ mm-video-v4l2/vidc/vdec/src/omx_vdec_msm8974.cpp
@@ -1585,24 +1585,28 @@ OMX_ERRORTYPE omx_vdec::is_video_session_supported()
         DEBUG_PRINT_HIGH("Set max_width=1280 & max_height=720 for H264 HP/MP");
     }
 
-    if ((drv_ctx.video_resolution.frame_width *
-                drv_ctx.video_resolution.frame_height >
-                m_decoder_capability.max_width *
-                m_decoder_capability.max_height) ||
-            (drv_ctx.video_resolution.frame_width*
-             drv_ctx.video_resolution.frame_height <
-             m_decoder_capability.min_width *
-             m_decoder_capability.min_height)) {
-        DEBUG_PRINT_ERROR(
-                "Unsupported WxH = (%u)x(%u) supported range is min(%u)x(%u) - max(%u)x(%u)",
-                drv_ctx.video_resolution.frame_width,
-                drv_ctx.video_resolution.frame_height,
-                m_decoder_capability.min_width,
-                m_decoder_capability.min_height,
-                m_decoder_capability.max_width,
-                m_decoder_capability.max_height);
+    if (drv_ctx.video_resolution.frame_width < m_decoder_capability.min_width ||
+        drv_ctx.video_resolution.frame_height < m_decoder_capability.min_height) {
+        DEBUG_PRINT_ERROR("Unsupported WxH = (%u)x(%u) less than min supported range (%u)x(%u)",
+                          drv_ctx.video_resolution.frame_width, drv_ctx.video_resolution.frame_height,
+                          m_decoder_capability.min_width, m_decoder_capability.min_height);
         return OMX_ErrorUnsupportedSetting;
     }
+
+    if (drv_ctx.video_resolution.frame_width > m_decoder_capability.max_width) {
+        DEBUG_PRINT_ERROR("Unsupported Width = (%u) greater than max supported Width (%u)",
+                          drv_ctx.video_resolution.frame_width, m_decoder_capability.max_width);
+        return OMX_ErrorUnsupportedSetting;
+    }
+
+    if (drv_ctx.video_resolution.frame_width * drv_ctx.video_resolution.frame_height >
+        m_decoder_capability.max_width * m_decoder_capability.max_height) {
+        DEBUG_PRINT_ERROR("Unsupported WxH = (%u)x(%u) max supported range (%u)x(%u)",
+                          drv_ctx.video_resolution.frame_width, drv_ctx.video_resolution.frame_height,
+                          m_decoder_capability.max_width, m_decoder_capability.max_height);
+        return OMX_ErrorUnsupportedSetting;
+    }
+
     DEBUG_PRINT_HIGH("video session supported");
     return OMX_ErrorNone;
 }
