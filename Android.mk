96b7b7f064b1cc40fe065e3fa3ed25a7dc43604e
Edward Wang <ewang@cyngn.com>
2016-02-17 16:35:28 -0800

    add 8909 into support list
    Issue-Id: FEIJ-6
    
    Change-Id: Ia7e68e6f597189495398a6fa58ac981230cc8b65

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
