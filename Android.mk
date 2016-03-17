3fa385a3bbb3e64c6e75e1c9372a3e69e3df57eb
dianlujitao <dianlujitao@gmail.com>
2016-03-18 00:51:43 +0800

    Fix subdirectory inclusion on CM
    * CM doesn't use MSM_VIDC_TARGET_LIST
    
    Change-Id: I0a3b748f3261b944c276b053bf96a79092273c5a

--- Android.mk
+++ Android.mk
@@ -7,9 +7,8 @@ include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
+ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
@@ -17,7 +16,6 @@ ifeq ($(TARGET_BOARD_PLATFORM),apq8084)
 include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
 endif
 
-endif
 endif
 endif
 endif
