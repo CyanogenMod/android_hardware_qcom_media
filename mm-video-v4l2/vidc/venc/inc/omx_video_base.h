bd10da8adffad851599703b97280dd1b67dd7156
Praveen Chavan <pchavan@codeaurora.org>
2016-03-21 12:43:42 -0700

    mm-video-v4l2: venc: add safety checks for freeing buffers
    Allow only up to 64 buffers on input/output port (since the
    allocation bitmap is only 64-wide).
    Add safety checks to free only as many buffers were allocated.
    
    Fixes: Heap Overflow and Possible Local Privilege Escalation in
    MediaServer (libOmxVenc problem)
    Bug: 27532497
    
    Change-Id: I31e576ef9dc542df73aa6b0ea113d72724b50fc6

--- mm-video-v4l2/vidc/venc/inc/omx_video_base.h
+++ mm-video-v4l2/vidc/venc/inc/omx_video_base.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
+Copyright (c) 2010-2016, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -136,9 +136,10 @@ static const char* MEM_DEVICE = "/dev/pmem_smipool";
         & BITMASK_FLAG(mIndex))
 #define BITMASK_ABSENT(mArray,mIndex) (((mArray)[BITMASK_OFFSET(mIndex)] \
             & BITMASK_FLAG(mIndex)) == 0x0)
-#ifdef _ANDROID_ICS_
+
 #define MAX_NUM_INPUT_BUFFERS 64
-#endif
+#define MAX_NUM_OUTPUT_BUFFERS 64
+
 void* message_thread(void *);
 
 enum omx_venc_extradata_types {
