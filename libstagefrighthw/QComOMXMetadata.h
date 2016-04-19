0cbd8487485ffdf4a4052160defcc77b9798b966
Praveen Chavan <pchavan@codeaurora.org>
2016-04-19 04:26:34 -0700

    mm-video: venc: update metadata-type used for HAL1 based recording
    Update metadata buffer type from kMetadataBufferTypeCameraSource to
    kMetadataBufferTypeNativeHandleSource as the latter is used by
    camera-HALv1 that resides in a separate process.
    
    Surface recording uses a different metadata as compared to
    legacy camera metadata (HAL1).Cast the gralloc-meta to
    appropriate struct to extract the handle
    
    Since buffer-size is not known conclusively at the allocation
    time of meta-handles, allow the least size to be allocated.
    But check the buffer size before accessing in ETB
    
    Bug: 26268807
    Change-Id: I2cb40889517b4d740763bbde09e2fa4646b919e1

--- libstagefrighthw/QComOMXMetadata.h
+++ libstagefrighthw/QComOMXMetadata.h
@@ -23,6 +23,16 @@
 
 namespace android {
 
+#ifdef USE_NATIVE_HANDLE_SOURCE
+    typedef struct encoder_nativehandle_buffer_type {
+        MetadataBufferType buffer_type;
+        union {
+            buffer_handle_t meta_handle;
+            uint64_t padding;
+        };
+    } encoder_nativehandle_buffer_type;
+#endif
+
     /*typedef enum {
         kMetadataBufferTypeCameraSource  = 0,
         kMetadataBufferTypeGrallocSource = 1,
