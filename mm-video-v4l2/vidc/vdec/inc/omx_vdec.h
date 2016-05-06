6f4a4bad7a65232a19ed91c6f731dee45391dd4e
Qiwei Liu <qiweil@codeaurora.org>
2016-05-06 11:38:26 +0800

    mm-video-v4l2: vide: vdec: modify refresh rate calculation
    Use float type for refresh rate.
    If omx client set valid fps, directly send it to display
    for the 1st fbd. Otherwise, calculate fps using fbd
    timestamps and send it to display when 2 fbds received
    and 30 fbds received (more accurate one).
    
    Change-Id: I291590f4c639dd164d88189d8bea0c4030062dee
    Signed-off-by: Qiwei Liu <qiweil@codeaurora.org>

--- mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
+++ mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -906,6 +906,8 @@ class omx_vdec: public qc_omx_component
         OMX_S64 prev_ts_actual;
         bool rst_prev_ts;
         OMX_U32 frm_int;
+        OMX_U32 m_fps_received;
+        bool m_drc_enable;
 
         struct vdec_allocatorproperty op_buf_rcnfg;
         bool in_reconfig;
