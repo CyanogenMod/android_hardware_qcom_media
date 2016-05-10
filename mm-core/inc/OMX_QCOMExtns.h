3407210d932fa03997c07fbf4d68092eff1bd313
Sanjay Singh <sisanj@codeaurora.org>
2016-05-10 12:50:02 -0700

    mm-video-v4l2: vidc: validate omx param/config data
    Check the sanity of config/param strcuture objects
    passed to get/set _ config()/parameter() methods.
    
    Bug: 27533317
    Security Vulnerability in MediaServer
    omx_vdec::get_config() Can lead to arbitrary write
    
    Change-Id: I6c3243afe12055ab94f1a1ecf758c10e88231809

--- mm-core/inc/OMX_QCOMExtns.h
+++ mm-core/inc/OMX_QCOMExtns.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
-Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
+Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -1336,6 +1336,8 @@ typedef struct QOMX_VIDEO_QUERY_DECODER_INSTANCES {
 } QOMX_VIDEO_QUERY_DECODER_INSTANCES;
 
 typedef struct QOMX_ENABLETYPE {
+    OMX_U32 nSize;
+    OMX_VERSIONTYPE nVersion;
     OMX_BOOL bEnable;
 } QOMX_ENABLETYPE;
 
@@ -1429,6 +1431,8 @@ typedef enum QOMX_VIDEO_PICTURETYPE {
 
 
 typedef struct QOMX_RECTTYPE {
+    OMX_U32 nSize;
+    OMX_VERSIONTYPE nVersion;
     OMX_S32 nLeft;
     OMX_S32 nTop;
     OMX_U32 nWidth;
