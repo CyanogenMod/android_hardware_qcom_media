4d6b67cc3635ba357edbafb611c23b4d1ccd4812
Praveen Chavan <pchavan@codeaurora.org>
2016-06-30 20:21:12 -0700

    mm-video-v4l2: venc: Disable VZIP SEI if this feature is disabled
    MPEG4Writer "assumes" that the encoded bitstream consists of only
    1 VCL NAL. If the encoder generates a non-VCL NAL, the resultant
    NALU in the track will appear erroneous to some players.
    Disable VZIP NALs since this feature is no needed
    
    Bug: 29515805
    Change-Id: I17f437e22a752ef1dc90b923471382aa0c6b82f1

--- msm8996/mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ msm8996/mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -5504,6 +5504,7 @@ bool venc_dev::venc_set_ratectrl_cfg(OMX_VIDEO_CONTROLRATETYPE eControlRate)
         rate_ctrl.rcmode = control.value;
     }
 
+#ifdef _VQZIP_
     if (eControlRate == OMX_Video_ControlRateVariable && (supported_rc_modes & RC_VBR_CFR)
         && m_sVenc_cfg.codectype == V4L2_PIX_FMT_H264) {
         /* Enable VQZIP SEI by default for camcorder RC modes */
@@ -5515,6 +5516,7 @@ bool venc_dev::venc_set_ratectrl_cfg(OMX_VIDEO_CONTROLRATETYPE eControlRate)
             DEBUG_PRINT_HIGH("Non-Fatal: Request to set VQZIP failed");
         }
     }
+#endif //_VQZIP_
 
     return status;
 }
