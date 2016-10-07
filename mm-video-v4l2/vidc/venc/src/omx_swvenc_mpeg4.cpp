6151dc4e930ae704e121d3b16005afa307b1bc42
Manikanta Sivapala <msivap@codeaurora.org>
2016-10-07 12:06:52 +0530

    swvenc : Sanitize buffer to native colorformat
      Buffers coming in from non-camera sources (e.g. apps) that aren't aware
      of vendor's color format flavours will provide input in standard NV12
      format.  Before providing it to sw encoder, we need to convert into native
      NV12 format.
    
    Change-Id: I424c5fa34014abc7ddd0df8b40c7e80088e68dd1

--- mm-video-v4l2/vidc/venc/src/omx_swvenc_mpeg4.cpp
+++ mm-video-v4l2/vidc/venc/src/omx_swvenc_mpeg4.cpp
@@ -2278,16 +2278,11 @@ bool omx_venc::dev_color_align(OMX_BUFFERHEADERTYPE *buffer,
 {
     ENTER_FUNC();
 
-    (void)buffer;
-    (void)width;
-    (void)height;
     if(secure_session) {
         DEBUG_PRINT_ERROR("Cannot align colors in secure session.");
         RETURN(OMX_FALSE);
     }
-
-    RETURN(true);
-    //return handle->venc_color_align(buffer, width,height);
+    return swvenc_color_align(buffer, width,height);
 }
 
 bool omx_venc::is_secure_session()
@@ -2979,4 +2974,42 @@ SWVENC_STATUS omx_venc::swvenc_set_intra_period
    }
 
    RETURN(Ret);
+}
+
+bool omx_venc::swvenc_color_align(OMX_BUFFERHEADERTYPE *buffer, OMX_U32 width,
+                        OMX_U32 height)
+{
+     OMX_U32 y_stride = VENUS_Y_STRIDE(COLOR_FMT_NV12, width),
+            y_scanlines = VENUS_Y_SCANLINES(COLOR_FMT_NV12, height),
+            uv_stride = VENUS_UV_STRIDE(COLOR_FMT_NV12, width),
+            uv_scanlines = VENUS_UV_SCANLINES(COLOR_FMT_NV12, height),
+            src_chroma_offset = width * height;
+
+    if (buffer->nAllocLen >= VENUS_BUFFER_SIZE(COLOR_FMT_NV12, width, height)) {
+        OMX_U8* src_buf = buffer->pBuffer, *dst_buf = buffer->pBuffer;
+        //Do chroma first, so that we can convert it in-place
+        src_buf += width * height;
+        dst_buf += y_stride * y_scanlines;
+        for (int line = height / 2 - 1; line >= 0; --line) {
+            memmove(dst_buf + line * uv_stride,
+                    src_buf + line * width,
+                    width);
+        }
+
+        dst_buf = src_buf = buffer->pBuffer;
+        //Copy the Y next
+        for (int line = height - 1; line > 0; --line) {
+            memmove(dst_buf + line * y_stride,
+                    src_buf + line * width,
+                    width);
+        }
+    } else {
+        DEBUG_PRINT_ERROR("Failed to align Chroma. from %u to %u : \
+                Insufficient bufferLen=%u v/s Required=%u",
+                (unsigned int)(width*height), (unsigned int)src_chroma_offset, (unsigned int)buffer->nAllocLen,
+                VENUS_BUFFER_SIZE(COLOR_FMT_NV12, width, height));
+        return false;
+    }
+
+    return true;
 }
